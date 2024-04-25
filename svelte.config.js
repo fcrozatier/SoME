52455f4958bdbc2179e40f60e35ab9d07f4fed58
fcrozatier <frederic.crozatier@protonmail.com>
2024-04-25 08:38:51 +0200

    add mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,23 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
+import rehypeTargetBlank from 'rehype-target-plugin';
+
+/**
+ * @type {import('mdsvex').MdsvexOptions}
+ */
+const mdsvexOptions = {
+	extensions: ['.md'],
+	layout: './src/lib/components/LayoutProse.svelte',
+	rehypePlugins: [rehypeTargetBlank]
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	extensions: ['.svelte', '.svx', '.md'],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
