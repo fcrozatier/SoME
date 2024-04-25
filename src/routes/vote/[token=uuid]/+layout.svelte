cf1b71cb676348d72c0a85522935aeea7f2fc033
fcrozatier <frederic.crozatier@protonmail.com>
2024-04-25 09:07:52 +0200

    keep app shell

--- src/routes/vote/[token=uuid]/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { COMPETITION_SHORT_NAME } from '$lib/config';
-</script>
-
-<svelte:head>
-	<title>Vote &middot; {COMPETITION_SHORT_NAME}</title>
-</svelte:head>
-
-<slot />
