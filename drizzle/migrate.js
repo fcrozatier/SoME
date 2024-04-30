4b1691aa7c86f88a0c408d3b8009552646986f3f
fcrozatier <frederic.crozatier@protonmail.com>
2024-04-30 11:59:25 +0200

    add drizzle scripts

---
+++ drizzle/migrate.js
@@ -0,0 +1,7 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
+import postgres from 'postgres';
+
+const migrationClient = postgres(process.env.DATABASE_URL, { max: 1 });
+await migrate(drizzle(migrationClient), { migrationsFolder: 'drizzle' });
+await migrationClient.end();
