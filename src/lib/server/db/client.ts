d8caa964d05668f35572e3d059584440c697abd1
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-01 11:30:29 +0200

    db client

---
+++ src/lib/server/db/client.ts
@@ -0,0 +1,6 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import { DATABASE_URL } from '$env/static/private';
+import postgres from 'postgres';
+
+const queryClient = postgres(DATABASE_URL, { max: 10 });
+export const db = drizzle(queryClient);
