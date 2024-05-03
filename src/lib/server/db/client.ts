ed6e266ecd56f1d6e45c7c3f63056a0f99976828
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-03 10:11:20 +0200

    update max number of connections

--- src/lib/server/db/client.ts
+++ src/lib/server/db/client.ts
@@ -3,5 +3,5 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import { DATABASE_URL } from '$env/static/private';
 import postgres from 'postgres';
 
-const queryClient = postgres(DATABASE_URL, { max: 10 });
+const queryClient = postgres(DATABASE_URL, { max: 22 });
 export const db = drizzle(queryClient, { schema });
