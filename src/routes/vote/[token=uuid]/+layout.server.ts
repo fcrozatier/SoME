0eb3d1ec1a0458357e679bcebe2aa41050b32eee
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-07 10:15:52 +0200

    do not rethrow

--- src/routes/vote/[token=uuid]/+layout.server.ts
+++ src/routes/vote/[token=uuid]/+layout.server.ts
@@ -7,15 +7,10 @@ import { eq } from 'drizzle-orm';
 export const load: LayoutServerLoad = async (event) => {
 	const { token } = event.params;
 
-	try {
-		// Find user
-		const user = (await db.select().from(users).where(eq(users.uid, token)).limit(1))[0];
+	// Find user
+	const user = (await db.select().from(users).where(eq(users.uid, token)).limit(1))[0];
 
-		if (!user) {
-			throw error(401, 'Invalid token: you can use the link you received by email to vote');
-		}
-	} catch (error) {
-		console.log(error);
-		throw error;
+	if (!user) {
+		throw error(401, 'Invalid token: you can use the link you received by email to vote');
 	}
 };
