613421fee89ac246426153801268e05a190925b8
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-14 08:58:55 +0200

    do not optimize mapbox

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,9 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	optimizeDeps: {
+		// mapbox is a dependency of bcrypt. As of 05/2024 generates errors in dev mode: No loader is configured for ".html"
+		exclude: ['@mapbox']
 	}
 });
