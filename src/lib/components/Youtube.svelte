5a07aded64791bfff40c635180d0fd918758f855
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-20 10:17:45 +0200

    add referrer policy

--- src/lib/components/Youtube.svelte
+++ src/lib/components/Youtube.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
 	export let src: string;
+	export let width = 420;
 </script>
 
 <iframe
-	width="420"
+	{width}
 	class="aspect-video rounded-lg"
 	{src}
 	title="YouTube video player"
 	frameborder="0"
 	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+	referrerpolicy="strict-origin-when-cross-origin"
 	allowfullscreen
 	loading="lazy"
-/>
+></iframe>
