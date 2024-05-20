8dd0f50dd5b290def25e5c52012dbae87f02513b
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-20 08:43:06 +0200

    save cookie

--- src/routes/vote/[token=uuid]/+layout.server.ts
+++ src/routes/vote/[token=uuid]/+layout.server.ts
@@ -3,6 +3,7 @@ import type { LayoutServerLoad } from './$types';
 import { db } from '$lib/server/db/client';
 import { users } from '$lib/server/db/schema';
 import { eq } from 'drizzle-orm';
+import { MAX_AGE } from '$lib/server/config';
 
 export const load: LayoutServerLoad = async (event) => {
 	const { token } = event.params;
@@ -11,6 +12,16 @@ export const load: LayoutServerLoad = async (event) => {
 	const user = (await db.select().from(users).where(eq(users.uid, token)).limit(1))[0];
 
 	if (!user) {
-		throw error(401, 'Invalid token: you can use the link you received by email to vote');
+		throw error(
+			401,
+			'Invalid token: you can use the link you received by email when registering to vote'
+		);
+	}
+
+	if (token !== event.cookies.get('token')) {
+		event.cookies.set('token', token, {
+			path: '/',
+			maxAge: MAX_AGE
+		});
 	}
 };
