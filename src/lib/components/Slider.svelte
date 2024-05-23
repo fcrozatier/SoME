11c372f05d9ad4d7a9b1a69acb08c7572f91b442
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-23 09:24:43 +0200

    make slider responsive

--- src/lib/components/Slider.svelte
+++ src/lib/components/Slider.svelte
@@ -1,57 +1,79 @@
 <script lang="ts">
 	export let name: string;
-	export let value = 5;
+	export let value: number;
 
-	export let labelTopLeft = '';
-	export let labelTopCenter = '';
-	export let labelTopRight = '';
-	export let labelBottomLeft = '';
-	export let labelBottomRight = '';
+	export let label1 = '';
+	export let label3 = '';
+	export let label5 = '';
+	export let label7 = '';
+	export let label9 = '';
+
+	function label(value: number) {
+		if (value < 1.5) return label1;
+		if (value < 2.5) return label3 + '-';
+		if (value < 3.5) return label3;
+		if (value < 4.5) return label5 + '-';
+		if (value < 5.5) return label5;
+		if (value < 6.5) return label5 + '+';
+		if (value < 7.5) return label7;
+		if (value < 8.5) return label7 + '+';
+		return label9;
+	}
 </script>
 
-<div class="w-full flex justify-between text-xs px-1 pb-1">
-	<button type="button" class="relative -left-1" on:click={() => (value = 1)}>{labelTopLeft}</button
+<div class="h-[400px] sm:h-auto py-10">
+	<div
+		class="-rotate-90 sm:rotate-0 w-[400px] sm:w-full h-[200px] sm:h-[100px] overflow-hidden -translate-x-20 translate-y-16 sm:translate-x-0 sm:translate-y-0"
 	>
-	<button type="button" class="hidden sm:block relative -left-1" on:click={() => (value = 3)}
-		>{labelBottomLeft}</button
-	>
-	<button type="button" class="relative -right-2" on:click={() => (value = 5)}
-		>{labelTopCenter}</button
-	>
-	<button type="button" class="hidden sm:block relative -right-4" on:click={() => (value = 7)}
-		>{labelBottomRight}</button
-	>
-	<button type="button" class="relatite -right-0" on:click={() => (value = 9)}
-		>{labelTopRight}</button
-	>
-</div>
-<input
-	id={name}
-	{name}
-	type="range"
-	min="1"
-	max="9"
-	step=".01"
-	class="range range-sm"
-	bind:value
-	class:range-error={value <= 3}
-	class:range-success={value >= 7}
-	class:range-warning={value > 3 && value < 7}
-	required
-/>
-<div class="w-full flex justify-between text-xs px-2">
-	{#each Array.from({ length: 9 }) as _, i}
-		<button type="button" on:click={() => (value = i + 1)}>{i + 1}</button>
-	{/each}
-</div>
-<div class="w-full flex justify-between text-xs px-1">
-	<span></span>
-	<button type="button" class="sm:hidden" on:click={() => (value = 3)}>{labelBottomLeft}</button>
-	<span></span>
-	<button type="button" class="sm:hidden" on:click={() => (value = 7)}>{labelBottomRight}</button>
-	<span></span>
+		<div
+			class="w-full flex justify-between text-xs px-1 pb-2 sm:translate-x-0 sm:translate-y-0 translate-x-[2.2rem] translate-y-14 scale-110 sm:scale-100"
+		>
+			<button
+				type="button"
+				class="rotate-90 sm:rotate-0 text-nowrap origin-left relative -left-1"
+				on:click={() => (value = 1)}>{label1}</button
+			>
+			<button
+				type="button"
+				class="rotate-90 sm:rotate-0 text-nowrap origin-left sm:block relative -left-1"
+				on:click={() => (value = 3)}>{label3}</button
+			>
+			<button
+				type="button"
+				class="rotate-90 sm:rotate-0 text-nowrap origin-left relative -right-2"
+				on:click={() => (value = 5)}>{label5}</button
+			>
+			<button
+				type="button"
+				class="rotate-90 sm:rotate-0 text-nowrap origin-left sm:block relative -right-4"
+				on:click={() => (value = 7)}>{label7}</button
+			>
+			<button
+				type="button"
+				class="rotate-90 sm:rotate-0 text-nowrap origin-left relatite -right-0"
+				on:click={() => (value = 9)}>{label9}</button
+			>
+		</div>
+		<div class="h-[500px]">
+			<input
+				id={name}
+				{name}
+				type="range"
+				min="1"
+				max="9"
+				step=".01"
+				class="range range-sm"
+				bind:value
+				class:range-error={value <= 3}
+				class:range-success={value >= 7}
+				class:range-warning={value > 3 && value < 7}
+				required
+			/>
+		</div>
+	</div>
 </div>
 
 <p class="">
-	Score: {value}
+	Your estimation: <span class="font-semibold">{value ? label(value) : 'Please make a choice'}</span
+	>
 </p>
