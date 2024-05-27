216337a011d8e05d8cbd106bd3d3ac0a1b0e7fae
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-27 10:14:13 +0200

    remove bcrypt

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,5 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
-	optimizeDeps: {
-		// mapbox is a dependency of bcrypt. As of 05/2024 generates errors in dev mode: No loader is configured for ".html"
-		exclude: ['@mapbox']
 	}
 });
