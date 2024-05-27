4fa24db99d39fcbfef007e6fdd2e321b7eb2c440
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-27 10:35:20 +0200

    bcrypt cleanup

--- src/lib/server/config.ts
+++ src/lib/server/config.ts
@@ -1,3 +1,2 @@
-export const BCRYPT_ROUNDS = 11;
 export const MAX_AGE = 1000 * 60 * 60 * 24 * 30 * 4; // 4 month(s)
 export const MAX_IMG_SIZE = 10 ** 6; // 1MB
--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,8 +1,7 @@
 import { fail, redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
-import { hash, compare } from 'bcrypt';
 import { ADMIN_PASSWORD } from '$env/static/private';
-import { BCRYPT_ROUNDS, MAX_AGE } from '$lib/server/config';
+import { MAX_AGE } from '$lib/server/config';
 import { PasswordForm, validateForm } from '$lib/server/validation';
 
 export const actions: Actions = {
@@ -12,21 +11,21 @@ export const actions: Actions = {
 		if (!validation.success) {
 			return fail(400, { invalid: true });
 		}
+		// TODO
+		// const adminHash = await hash(ADMIN_PASSWORD);
+		// const passwordValid = await compare(validation.data.password, adminHash);
 
-		const adminHash = await hash(ADMIN_PASSWORD, BCRYPT_ROUNDS);
-		const passwordValid = await compare(validation.data.password, adminHash);
+		// if (!passwordValid) {
+		// 	return fail(400, { invalid: true });
+		// }
 
-		if (!passwordValid) {
-			return fail(400, { invalid: true });
-		}
-
-		cookies.set('password', adminHash, {
-			httpOnly: true,
-			path: '/',
-			maxAge: MAX_AGE,
-			sameSite: 'strict',
-			secure: true
-		});
+		// cookies.set('password', adminHash, {
+		// 	httpOnly: true,
+		// 	path: '/',
+		// 	maxAge: MAX_AGE,
+		// 	sameSite: 'strict',
+		// 	secure: true
+		// });
 
 		throw redirect(303, '/admin');
 	}
