3f8e4216766b90a5f35d79b75fd0fdbdf47449b8
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-30 10:40:44 +0200

    thumbnail component

---
+++ src/lib/components/Thumbnail.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { PUBLIC_S3_BUCKET, PUBLIC_S3_ENDPOINT } from '$env/static/public';
+
+	export let uid: string;
+	export let width = 480;
+</script>
+
+<img
+	class="my-0 max-w-full rounded-lg aspect-video"
+	src={`https://${PUBLIC_S3_BUCKET}.${PUBLIC_S3_ENDPOINT.replace('https://', '')}/${uid}`}
+	alt="thumbnail"
+	{width}
+/>
