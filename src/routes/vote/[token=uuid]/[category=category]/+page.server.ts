20681593077391a1874166f7f860fb87411665c4
fcrozatier <frederic.crozatier@protonmail.com>
2024-06-13 09:33:50 +0200

    fix query

--- src/routes/vote/[token=uuid]/[category=category]/+page.server.ts
+++ src/routes/vote/[token=uuid]/[category=category]/+page.server.ts
@@ -40,15 +40,16 @@ export const load: PageServerLoad = async (event) => {
 					select entry_uid from cache
 					) as summation
 				group by entry_uid
-			)
+			),
 
-			select uid, title, description, entries.category, url, thumbnail, total.count
-			from entries
-			left join total
-			on entries.uid=total.entry_uid
+			selection as (
+				select uid, title, description, entries.category, url, thumbnail, coalesce(total.count, 0) as score
+				from entries
+				left join total
+				on entries.uid=total.entry_uid
 
-			where
-				case when (select count(*) > 0 from cached)
+				where
+					case when (select count(*) > 0 from cached)
 				then
 					uid in (select entry_uid from cached)
 				else
@@ -58,9 +59,26 @@ export const load: PageServerLoad = async (event) => {
 					and uid not in (select entry_uid from skips where skips.user_uid=${token})
 					and uid not in (select entry_uid from flags where flags.user_uid=${token})
 					and uid not in (select entry_uid from ${usersToEntries} where ${usersToEntries.userUid}=${token})
-			  end
+				end
 
-			order by total.count nulls first
+				order by total.count nulls first
+			),
+
+			maximum as (
+				select max(score) as max from selection
+			),
+
+			cutoff as (
+			  select floor(random() * (select sum(max-score) from selection, maximum)) as value
+			),
+
+			partial as (
+				select uid, sum(max-score) over (
+					order by score rows between unbounded preceding and current row) sum from selection, maximum, cutoff
+			)
+
+			select * from (selection join partial on selection.uid=partial.uid), cutoff
+			where partial.sum >= cutoff.value
 			limit 1;
 		`);
 
