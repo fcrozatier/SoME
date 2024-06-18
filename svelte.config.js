df344296aed4f49d6c57471088c3f360d55bc4db
fcrozatier <frederic.crozatier@protonmail.com>
2024-06-18 10:43:25 +0200

    remove comments

--- svelte.config.js
+++ svelte.config.js
@@ -13,8 +13,6 @@ const mdsvexOptions = {
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	extensions: ['.svelte', '.svx', '.md'],
 	preprocess: [
 		vitePreprocess(),
@@ -23,9 +21,6 @@ const config = {
 	],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	}
 };
