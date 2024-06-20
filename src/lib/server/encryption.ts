0086b8ed097166ac09f0d19fa864da99c87250a8
fcrozatier <frederic.crozatier@protonmail.com>
2024-06-20 11:52:42 +0200

    coherent iv between builds

--- src/lib/server/encryption.ts
+++ src/lib/server/encryption.ts
@@ -1,14 +1,11 @@
-import { SECRET_KEY } from '$env/static/private';
-import { createCipheriv, createDecipheriv, randomBytes } from 'crypto';
-
-const iv = randomBytes(12).toString('base64');
-console.log('initialization vector', iv);
+import { IV, SECRET_KEY } from '$env/static/private';
+import { createCipheriv, createDecipheriv } from 'crypto';
 
 export const encrypt = (plaintext: string) => {
 	const cipher = createCipheriv(
 		'aes-256-gcm',
 		Buffer.from(SECRET_KEY, 'base64'),
-		Buffer.from(iv, 'base64')
+		Buffer.from(IV, 'base64')
 	);
 
 	let cipherText = cipher.update(plaintext, 'utf-8', 'base64');
@@ -22,7 +19,7 @@ export const decrypt = (cipherText: string, tag: string) => {
 	const decipher = createDecipheriv(
 		'aes-256-gcm',
 		Buffer.from(SECRET_KEY, 'base64'),
-		Buffer.from(iv, 'base64')
+		Buffer.from(IV, 'base64')
 	);
 	decipher.setAuthTag(Buffer.from(tag, 'base64'));
 
