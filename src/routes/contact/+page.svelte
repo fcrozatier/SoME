5943842fdc222cc14861658c2378d866a1ad5b5f
fcrozatier <frederic.crozatier@protonmail.com>
2024-06-24 09:45:52 +0200

    add email

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -11,9 +11,7 @@
 
 	<p>
 		This is a community-driven event, if you have questions or suggestions feel free to reach out
-		directly on <a href="https://discord.gg/WZvZMVsXXR">Discord</a>
-	</p>
-	<!-- <address>
+		directly on <a href="https://discord.gg/WZvZMVsXXR">Discord</a> or by email at
 		<a href="mailto:3b1b.some@gmail.com"> 3b1b.some@gmail.com</a>
-	</address> -->
+	</p>
 </article>
