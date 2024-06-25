4c57f0d5d6c2682ca40d1cb80ff2727a6d3ee616
fcrozatier <frederic.crozatier@protonmail.com>
2024-06-25 09:28:42 +0200

    refactor snippet

--- src/lib/components/NewVote.svelte
+++ src/lib/components/NewVote.svelte
@@ -1,26 +1,26 @@
 <script lang="ts">
 	import { categories } from '$lib/config';
+	import type { Page } from '@sveltejs/kit';
+	import type { Readable } from 'svelte/store';
 
-	export let page;
+	export let page: Readable<Page<Record<string, string>>>;
 	export let displayCategories: 'all' | 'others-only' = 'all';
 </script>
 
-<!-- Force reload to grab a new pair of entries -->
 {#if displayCategories !== 'others-only'}
 	<p>
-		<a
-			class="btn-primary btn-lg btn"
-			href={`/vote/${$page.params.token}/${$page.params.category}`}
-			data-sveltekit-reload>New vote</a
-		>
+		<button class="btn-primary btn-lg btn">New vote</button>
 	</p>
 	<p>Or change category:</p>
 {/if}
 <p class="flex">
 	{#each categories as category}
 		{#if category !== $page.params.category}
-			<a class="btn" href={`/vote/${$page.params.token}/${category}`} data-sveltekit-reload
-				>Go to {category} entries</a
+			<!-- Force reload to grab a new entry -->
+			<a
+				class="btn"
+				href={`/vote/${$page.params.token}/${category}?screen=ok`}
+				data-sveltekit-reload>Go to {category} entries</a
 			>
 		{/if}
 	{/each}
