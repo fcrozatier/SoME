b6f4d8c53247d5cae76b1420c19f02fa4e191435
fcrozatier <frederic.crozatier@protonmail.com>
2024-06-26 10:42:48 +0200

    query for day 3

---
+++ src/lib/server/algo/day3.pgsql
@@ -0,0 +1,59 @@
+-- Choose an entry at random with probability weighted by its median score with a (utility) boost based on the std
+
+with cached as (
+  select entry_uid
+  from cache join entries on cache.entry_uid=entries.uid
+  where cache.user_uid='70601724-87ef-4b68-87dc-c9cbb06159fc'
+  and cache.category='video'
+),
+
+scores as (
+  select entry_uid, count(*), percentile_cont(0.5) within group (order by score) as median,
+  stddev_samp(score) as std
+  from (select entry_uid, score from votes) as Q
+  group by entry_uid
+),
+
+multiplier as (
+  select entry_uid, count(*) as value
+  from votes
+  where created_at < '2024-08-20'
+  group by entry_uid
+),
+
+selection as (
+  select uid, title, entries.category, coalesce(scores.median, 9) + coalesce(multiplier.value, 0) * coalesce(scores.std, 0) / coalesce(scores.count, 1) as score
+  from entries
+  left join multiplier
+  on entries.uid=multiplier.entry_uid
+  left join scores
+  on entries.uid=scores.entry_uid
+
+  where
+    case when (select count(*) > 0 from cached)
+    then
+      uid in (select entry_uid from cached)
+    else
+      entries.category='video'
+      and active='true'
+      and uid not in (select entry_uid from votes where votes.user_uid='70601724-87ef-4b68-87dc-c9cbb06159fc')
+      and uid not in (select entry_uid from skips where skips.user_uid='70601724-87ef-4b68-87dc-c9cbb06159fc')
+      and uid not in (select entry_uid from flags where flags.user_uid='70601724-87ef-4b68-87dc-c9cbb06159fc')
+      and uid not in (select entry_uid from user_to_entry where user_to_entry.user_uid='70601724-87ef-4b68-87dc-c9cbb06159fc')
+    end
+
+  order by score nulls last
+),
+
+cutoff as (
+  select (random() * (select sum(score) from selection)) as value
+),
+
+partial as (
+  select uid, sum(score) over (
+    order by score rows between unbounded preceding and current row) sum from selection
+)
+
+select * from (selection join partial on selection.uid=partial.uid), cutoff
+where partial.sum >= cutoff.value
+limit 1;
--- src/lib/server/algo/queries.ts
+++ src/lib/server/algo/queries.ts
@@ -127,3 +127,71 @@ export function query2(token: string, category: string) {
 			limit 1;
 		`;
 }
+
+const multiplier_start_date = '2024-08-20';
+
+/**
+ * Select entries at random with probability weighted by median score with a (utility) boost based on the std
+ * We can still accept late entries at this stage (but shouldn't), they will be prioritized
+ */
+export function query3(token: string, category: string) {
+	return sql`
+			with cached as (
+				select entry_uid
+				from cache join entries on cache.entry_uid=entries.uid
+				where cache.user_uid=${token}
+				and cache.category=${category}
+			),
+
+			scores as (
+				select entry_uid, count(*), percentile_cont(0.5) within group (order by score) as median,
+				stddev_samp(score) as std
+				from (select entry_uid, score from votes) as Q
+				group by entry_uid
+			),
+
+			multiplier as (
+				select entry_uid, count(*) as value
+				from votes
+				where created_at < ${multiplier_start_date}
+				group by entry_uid
+			),
+
+			selection as (
+				select uid, title, entries.category, coalesce(scores.median, 9) + coalesce(multiplier.value, 0) * coalesce(scores.std, 0) / coalesce(scores.count, 1) as score
+				from entries
+				left join multiplier
+				on entries.uid=multiplier.entry_uid
+				left join scores
+				on entries.uid=scores.entry_uid
+
+				where
+					case when (select count(*) > 0 from cached)
+					then
+						uid in (select entry_uid from cached)
+					else
+						entries.category=${category}
+						and active='true'
+						and uid not in (select entry_uid from votes where votes.user_uid=${token})
+						and uid not in (select entry_uid from skips where skips.user_uid=${token})
+						and uid not in (select entry_uid from flags where flags.user_uid=${token})
+						and uid not in (select entry_uid from ${usersToEntries} where ${usersToEntries.userUid}=${token})
+					end
+
+				order by score nulls last
+			),
+
+			cutoff as (
+				select (random() * (select sum(score) from selection)) as value
+			),
+
+			partial as (
+				select uid, sum(score) over (
+					order by score rows between unbounded preceding and current row) sum from selection
+			)
+
+			select * from (selection join partial on selection.uid=partial.uid), cutoff
+			where partial.sum >= cutoff.value
+			limit 1;
+		`;
+}
