d7dee2e9a0ee28d1959da10f9d2a27e2a191e7f9
fcrozatier <frederic.crozatier@protonmail.com>
2024-07-02 10:31:34 +0200

    katex component

---
+++ src/lib/components/K.svelte
@@ -0,0 +1,16 @@
+<script context="module" lang="ts">
+	import type { KatexOptions } from 'katex';
+	import katex from 'katex';
+</script>
+
+<script lang="ts">
+	export let displayMode = false;
+	export let nowrap = false;
+	export let tex: string;
+	export let options: KatexOptions = { output: 'html' };
+</script>
+
+<!-- SSR with renderToString -->
+<span class:whitespace-nowrap={nowrap}>
+	{@html katex.renderToString(tex, { displayMode, throwOnError: false, ...options })}
+</span>
