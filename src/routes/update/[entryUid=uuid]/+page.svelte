680b15d83f5622ad30c74944fe5dead06deff960
fcrozatier <frederic.crozatier@protonmail.com>
2024-07-25 11:26:40 +0200

    show toast on success

--- src/routes/update/[entryUid=uuid]/+page.svelte
+++ src/routes/update/[entryUid=uuid]/+page.svelte
@@ -2,6 +2,7 @@
 	import { enhance } from '$app/forms';
 	import { page } from '$app/stores';
 	import { PUBLIC_S3_BUCKET, PUBLIC_S3_ENDPOINT } from '$env/static/public';
+	import { newToast } from '$lib/components/Toasts.svelte';
 	import { COMPETITION_FULL_NAME, COMPETITION_SHORT_NAME, categories } from '$lib/config';
 	import { YOUTUBE_EMBEDDABLE, normalizeYoutubeLink, registrationOpen, voteOpen } from '$lib/utils';
 	import { tick } from 'svelte';
@@ -64,258 +65,254 @@
 </svelte:head>
 
 <article class="layout-prose">
-	{#if form?.success}
-		<h2>Entry updated!</h2>
+	<h2>Update your entry</h2>
 
-		<p>See you in the voting phase!</p>
-		<p><a href="/">Home</a></p>
-	{:else}
-		<h2>Update your entry</h2>
+	<p>You can update your entry at any time during the registration period.</p>
+	<form
+		method="post"
+		enctype="multipart/form-data"
+		use:enhance={async ({ submitter, formData, cancel }) => {
+			formData.append('others', JSON.stringify(otherContributors));
+			formData.set('userType', 'creator');
+			submitter?.setAttribute('disabled', 'on');
 
-		<p>You can update your entry at any time during the registration period.</p>
-		<form
-			method="post"
-			enctype="multipart/form-data"
-			use:enhance={async ({ submitter, formData, cancel }) => {
-				formData.append('others', JSON.stringify(otherContributors));
-				formData.set('userType', 'creator');
-				submitter?.setAttribute('disabled', 'on');
-
-				if (registrationOpen() && voteOpen() && data.url !== normalizeYoutubeLink(link)) {
-					const result = await showModal();
-					if (result === 'cancel') {
-						submitter?.removeAttribute('disabled');
-						cancel();
-					}
-				}
-				return async ({ update }) => {
-					await update();
+			if (registrationOpen() && voteOpen() && data.url !== normalizeYoutubeLink(link)) {
+				const result = await showModal();
+				if (result === 'cancel') {
 					submitter?.removeAttribute('disabled');
-				};
-			}}
-		>
-			<div class="form-control max-w-md">
-				<label for="email" class="label">
-					<span class="label-text"> Email </span>
-				</label>
-				<input
-					id="email"
-					type="email"
-					name="email"
-					placeholder="john@gmail.com"
-					class="input-bordered input w-full"
-					bind:value={email}
-					required
-				/>
-				{#if form?.fieldErrors?.email}
-					<span class="block text-error">{form.fieldErrors.email.join(', ')}</span>
-				{/if}
-			</div>
-
-			{#each otherContributors as _, i}
-				<div class="form-control max-w-md">
-					<label for="email-{i}" class="label">
-						<span class="label-text">Email {i + 2}</span>
-					</label>
-					<div class="flex items-center gap-2">
-						<input
-							id="email-{i}"
-							type="email"
-							name="email_{i}"
-							class="input-bordered input w-full"
-							bind:value={otherContributors[i]}
-							required
-						/>
-						<button
-							type="button"
-							class="btn-outline btn-xs btn-circle btn opacity-80"
-							on:click={() => {
-								otherContributors.splice(i, 1);
-								otherContributors = otherContributors;
-							}}>&cross;</button
-						>
-					</div>
-				</div>
-			{/each}
-			<p class="flex items-center gap-2 text-sm text-gray-500">
-				Add contributor
-				<button
-					type="button"
-					class="btn-outline btn-sm btn-circle btn opacity-80"
-					on:click={addContributor}
-				>
-					+</button
-				>
-			</p>
-
-			{#if form?.undeliverable}
-				<span class="block text-error"
-					>undeliverable email{otherContributors.length > 0 ? ': ' + form.undeliverable : ''}</span
-				>
+					cancel();
+				}
+			}
+			return async ({ update, result }) => {
+				if (result.type === 'success') {
+					newToast({ content: 'Entry updated!', type: 'success' });
+				}
+				await update({ reset: false });
+				submitter?.removeAttribute('disabled');
+			};
+		}}
+	>
+		<div class="form-control max-w-md">
+			<label for="email" class="label">
+				<span class="label-text"> Email </span>
+			</label>
+			<input
+				id="email"
+				type="email"
+				name="email"
+				placeholder="john@gmail.com"
+				class="input-bordered input w-full"
+				bind:value={email}
+				required
+			/>
+			{#if form?.fieldErrors?.email}
+				<span class="block text-error">{form.fieldErrors.email.join(', ')}</span>
 			{/if}
+		</div>
 
+		{#each otherContributors as _, i}
 			<div class="form-control max-w-md">
-				<label for="category" class="label">
-					<span class="label-text"> Category </span>
+				<label for="email-{i}" class="label">
+					<span class="label-text">Email {i + 2}</span>
 				</label>
-				<select
-					id="category"
-					name="category"
-					class="select-bordered select w-full"
-					bind:value={category}
-					required
-				>
-					{#each categories as category}
-						<option value={category}>{category}</option>
-					{/each}
-				</select>
-				{#if form?.fieldErrors?.category}
-					<span class="block text-error">{form.fieldErrors.category.join(', ')}</span>
-				{/if}
-			</div>
-
-			<div class="form-control max-w-md">
-				<label for="title" class="label">
-					<span class="label-text">Title</span>
-				</label>
-				<input
-					id="title"
-					type="text"
-					name="title"
-					class="input-bordered input w-full"
-					bind:value={title}
-					required
-				/>
-				{#if form?.fieldErrors?.title}
-					<span class="block text-error">{form.fieldErrors.title.join(', ')}</span>
-				{/if}
-			</div>
-
-			<div class="form-control max-w-md">
-				<label for="description" class="label">
-					<span class="label-text">Description</span>
-				</label>
-				<textarea
-					id="description"
-					name="description"
-					class="textarea-bordered textarea text-base"
-					minlength="10"
-					maxlength="5000"
-					rows="8"
-					bind:value={description}
-					required
-				/>
-				<div class="label">
-					<span class="label-text-alt text-error">
-						{#if form?.fieldErrors?.description}
-							{form.fieldErrors.description.join(', ')}
-						{/if}
-					</span>
-					<span class="label-text-alt">{description.length}/5000</span>
-				</div>
-			</div>
-
-			<div class="form-control max-w-md">
-				<label for="link" class="label">
-					<span class="label-text"> Link </span>
-				</label>
-				<input
-					id="link"
-					type="url"
-					name="link"
-					placeholder="https://"
-					class="input-bordered input w-full"
-					bind:value={link}
-					disabled={!registrationOpen()}
-				/>
-				{#if form?.fieldErrors?.link}
-					<span class="block text-error">{form.fieldErrors.link.join(', ')} </span>
-				{:else if form?.linkExists}
-					<span class="block text-error">entry already registered</span>
-				{/if}
-			</div>
-
-			<div>
-				{#if !YOUTUBE_EMBEDDABLE.test(data.url)}
-					<p>Current thumbnail</p>
-					<img
-						class="my-0 max-w-full rounded-lg"
-						src={`https://${PUBLIC_S3_BUCKET}.${PUBLIC_S3_ENDPOINT.replace('https://', '')}/${data.thumbnail}`}
-						alt="thumbnail"
-						width="480"
-						height="270"
-					/>
-				{/if}
-			</div>
-
-			{#if link && !YOUTUBE_EMBEDDABLE.test(link)}
-				<div class="form-control max-w-md">
-					<label for="thumbnail" class="label">
-						<span class="label-text">Thumbnail</span>
-						<span class="label-text-alt">Recommended ratio 16:9</span>
-					</label>
+				<div class="flex items-center gap-2">
 					<input
-						id="thumbnail"
-						type="file"
-						accept="image/*"
-						name="thumbnail"
-						class="file-input input-bordered"
+						id="email-{i}"
+						type="email"
+						name="email_{i}"
+						class="input-bordered input w-full"
+						bind:value={otherContributors[i]}
 						required
 					/>
-					{#if form?.fieldErrors?.thumbnail}
-						<span class="block text-error">{form.fieldErrors.thumbnail.join(', ')} </span>
-					{:else if form?.thumbnailRequired}
-						<span class="block text-error">A thumbnail is required</span>
-					{/if}
+					<button
+						type="button"
+						class="btn-outline btn-xs btn-circle btn opacity-80"
+						on:click={() => {
+							otherContributors.splice(i, 1);
+							otherContributors = otherContributors;
+						}}>&cross;</button
+					>
 				</div>
-			{/if}
+			</div>
+		{/each}
+		<p class="flex items-center gap-2 text-sm text-gray-500">
+			Add contributor
+			<button
+				type="button"
+				class="btn-outline btn-sm btn-circle btn opacity-80"
+				on:click={addContributor}
+			>
+				+</button
+			>
+		</p>
 
+		{#if form?.undeliverable}
+			<span class="block text-error"
+				>undeliverable email{otherContributors.length > 0 ? ': ' + form.undeliverable : ''}</span
+			>
+		{/if}
+
+		<div class="form-control max-w-md">
+			<label for="category" class="label">
+				<span class="label-text"> Category </span>
+			</label>
+			<select
+				id="category"
+				name="category"
+				class="select-bordered select w-full"
+				bind:value={category}
+				required
+			>
+				{#each categories as category}
+					<option value={category}>{category}</option>
+				{/each}
+			</select>
+			{#if form?.fieldErrors?.category}
+				<span class="block text-error">{form.fieldErrors.category.join(', ')}</span>
+			{/if}
+		</div>
+
+		<div class="form-control max-w-md">
+			<label for="title" class="label">
+				<span class="label-text">Title</span>
+			</label>
+			<input
+				id="title"
+				type="text"
+				name="title"
+				class="input-bordered input w-full"
+				bind:value={title}
+				required
+			/>
+			{#if form?.fieldErrors?.title}
+				<span class="block text-error">{form.fieldErrors.title.join(', ')}</span>
+			{/if}
+		</div>
+
+		<div class="form-control max-w-md">
+			<label for="description" class="label">
+				<span class="label-text">Description</span>
+			</label>
+			<textarea
+				id="description"
+				name="description"
+				class="textarea-bordered textarea text-base"
+				minlength="10"
+				maxlength="5000"
+				rows="8"
+				bind:value={description}
+				required
+			/>
+			<div class="label">
+				<span class="label-text-alt text-error">
+					{#if form?.fieldErrors?.description}
+						{form.fieldErrors.description.join(', ')}
+					{/if}
+				</span>
+				<span class="label-text-alt">{description.length}/5000</span>
+			</div>
+		</div>
+
+		<div class="form-control max-w-md">
+			<label for="link" class="label">
+				<span class="label-text"> Link </span>
+			</label>
+			<input
+				id="link"
+				type="url"
+				name="link"
+				placeholder="https://"
+				class="input-bordered input w-full"
+				bind:value={link}
+				disabled={!registrationOpen()}
+			/>
+			{#if form?.fieldErrors?.link}
+				<span class="block text-error">{form.fieldErrors.link.join(', ')} </span>
+			{:else if form?.linkExists}
+				<span class="block text-error">entry already registered</span>
+			{/if}
+		</div>
+
+		<div>
+			{#if !YOUTUBE_EMBEDDABLE.test(data.url)}
+				<p>Current thumbnail</p>
+				<img
+					class="my-0 max-w-full rounded-lg"
+					src={`https://${PUBLIC_S3_BUCKET}.${PUBLIC_S3_ENDPOINT.replace('https://', '')}/${data.thumbnail}`}
+					alt="thumbnail"
+					width="480"
+					height="270"
+				/>
+			{/if}
+		</div>
+
+		{#if link && !YOUTUBE_EMBEDDABLE.test(link)}
 			<div class="form-control max-w-md">
-				<label for="rules" class="label justify-normal gap-4">
-					<input id="rules" type="checkbox" name="rules" class="checkbox" required />
-					<span class="label-text">
-						I've read the <a href="/#rules">rules</a> of the competition
-					</span>
+				<label for="thumbnail" class="label">
+					<span class="label-text">Thumbnail</span>
+					<span class="label-text-alt">Recommended ratio 16:9</span>
 				</label>
-				{#if form?.fieldErrors?.rules}
-					<span class="block text-error">{form.fieldErrors.rules.join(', ')} </span>
+				<input
+					id="thumbnail"
+					type="file"
+					accept="image/*"
+					name="thumbnail"
+					class="file-input input-bordered"
+					required
+				/>
+				{#if form?.fieldErrors?.thumbnail}
+					<span class="block text-error">{form.fieldErrors.thumbnail.join(', ')} </span>
+				{:else if form?.thumbnailRequired}
+					<span class="block text-error">A thumbnail is required</span>
 				{/if}
 			</div>
+		{/if}
 
-			<div class="form-control max-w-md">
-				<label for="copyright" class="label items-start justify-normal gap-4">
-					<input id="copyright" type="checkbox" name="copyright" class="checkbox" required />
-					<span class="label-text">
-						I have permission to use all material contained in my submission for the {COMPETITION_FULL_NAME}.
-						<ul class="relative right-6 list-outside">
-							<li>
-								<a href="/content-policy#fair-use"
-									>Copyrighted material policy and fair use guidelines</a
-								>
-							</li>
-							<li><a href="/content-policy#cc">Creative Commons guidelines</a></li>
-							<li><a href="/content-policy#ai">AI policy</a></li>
-						</ul>
-					</span>
-				</label>
-				{#if form?.fieldErrors?.copyright}
-					<span class="block text-error">{form.fieldErrors.copyright.join(', ')} </span>
-				{/if}
-			</div>
-
-			{#if form?.fieldErrors || $page.status !== 200}
-				<p class="block text-error">
-					Something went wrong. {form?.message || ''}
-				</p>
+		<div class="form-control max-w-md">
+			<label for="rules" class="label justify-normal gap-4">
+				<input id="rules" type="checkbox" name="rules" class="checkbox" required />
+				<span class="label-text">
+					I've read the <a href="/#rules">rules</a> of the competition
+				</span>
+			</label>
+			{#if form?.fieldErrors?.rules}
+				<span class="block text-error">{form.fieldErrors.rules.join(', ')} </span>
 			{/if}
-			<p>
-				<button class="btn-primary btn block"> Submit </button>
+		</div>
+
+		<div class="form-control max-w-md">
+			<label for="copyright" class="label items-start justify-normal gap-4">
+				<input id="copyright" type="checkbox" name="copyright" class="checkbox" required />
+				<span class="label-text">
+					I have permission to use all material contained in my submission for the {COMPETITION_FULL_NAME}.
+					<ul class="relative right-6 list-outside">
+						<li>
+							<a href="/content-policy#fair-use"
+								>Copyrighted material policy and fair use guidelines</a
+							>
+						</li>
+						<li><a href="/content-policy#cc">Creative Commons guidelines</a></li>
+						<li><a href="/content-policy#ai">AI policy</a></li>
+					</ul>
+				</span>
+			</label>
+			{#if form?.fieldErrors?.copyright}
+				<span class="block text-error">{form.fieldErrors.copyright.join(', ')} </span>
+			{/if}
+		</div>
+
+		{#if form?.fieldErrors || $page.status !== 200}
+			<p class="block text-error">
+				Something went wrong. {form?.message || ''}
 			</p>
-			<p class="text-sm">
-				<a href="/gdpr">Privacy policy</a>
-			</p>
-		</form>
-	{/if}
+		{/if}
+		<p>
+			<button class="btn-primary btn block"> Submit </button>
+		</p>
+		<p class="text-sm">
+			<a href="/gdpr">Privacy policy</a>
+		</p>
+	</form>
 </article>
 
 <dialog class="mb-auto" bind:this={confirmDialog} role="alertdialog">
