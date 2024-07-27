cd02c354b2dd3682559097686be8191c93382f60
fcrozatier <frederic.crozatier@protonmail.com>
2024-07-27 14:22:50 +0200

    export db client

--- src/lib/server/db/client.ts
+++ src/lib/server/db/client.ts
@@ -1,7 +1,7 @@
-import * as schema from './schema';
-import { drizzle } from 'drizzle-orm/postgres-js';
 import { DATABASE_URL } from '$env/static/private';
+import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
+import * as schema from './schema';
 
-const queryClient = postgres(DATABASE_URL, { max: 22 });
-export const db = drizzle(queryClient, { schema });
+export const client = postgres(DATABASE_URL, { max: 22 });
+export const db = drizzle(client, { schema });
