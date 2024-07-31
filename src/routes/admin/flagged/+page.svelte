db8d13b301a938ae8ca5ee8aa63a0874ac50ad55
fcrozatier <frederic.crozatier@protonmail.com>
2024-07-31 10:33:02 +0200

    align left

--- src/routes/admin/flagged/+page.svelte
+++ src/routes/admin/flagged/+page.svelte
@@ -11,6 +11,36 @@
 <article class="mx-auto w-4/5 max-w-5xl overflow-x-auto">
 	<h2>Reported entries</h2>
 
+	<table class="w-full">
+		<thead>
+			<tr class="px-6">
+				<th class="flex items-center"
+					><input id="all" type="checkbox" class="checkbox" use:toggleSelectAll /></th
+				>
+				<th class="text-left">Entry</th>
+				<th class="text-left">Reason</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each data.flagged as entry}
+				<tr class="px-6 py-2">
+					<td class="flex items-center"
+						><input
+							type="checkbox"
+							class="checkbox"
+							name="selected"
+							value={entry.uid}
+							bind:group={selected}
+						/></td
+					>
+					<td><a class="capitalize" href={entry.url} target="_blank">{entry.title}</a></td>
+					<td><span class="">{entry.reason}</span></td>
+				</tr>
+			{:else}
+				<p>No entries to review</p>
+			{/each}
+		</tbody>
+	</table>
 	<form
 		method="post"
 		use:enhance={({ formData }) => {
@@ -32,53 +62,21 @@
 			};
 		}}
 	>
-		<table class="w-full">
-			<thead>
-				<tr class="px-6">
-					<th class="flex items-center"
-						><input id="all" type="checkbox" class="checkbox" use:toggleSelectAll /></th
-					>
-					<th>Entry</th>
-					<th>Reason</th>
-				</tr>
-			</thead>
-			<tbody>
-				{#each data.flagged as entry}
-					<tr class="px-6 py-2">
-						<td class="flex items-center"
-							><input
-								type="checkbox"
-								class="checkbox"
-								name="selected"
-								value={entry.uid}
-								bind:group={selected}
-							/></td
-						>
-						<td><a class="capitalize" href={entry.url} target="_blank">{entry.title}</a></td>
-						<td><span class="">{entry.reason}</span></td>
-					</tr>
-				{:else}
-					<p>No entries to review</p>
-				{/each}
-			</tbody>
-		</table>
-		<div>
-			<div class="flex gap-4">
-				<button type="submit" formaction="?/ignore" class="btn" disabled={!selected.length}
-					>Ignore</button
-				>
-				<button
-					type="submit"
-					formaction="?/deactivate"
-					class="btn-error btn"
-					disabled={!selected.length}>Deactivate</button
-				>
-			</div>
-			<p class="text-sm">
-				Deactivation is not destructive: the creator(s) will still have access to the feedbacks, but
-				the entry will not receive any new votes
-			</p>
+		<div class="flex gap-4">
+			<button type="submit" formaction="?/ignore" class="btn" disabled={!selected.length}
+				>Ignore</button
+			>
+			<button
+				type="submit"
+				formaction="?/deactivate"
+				class="btn-error btn"
+				disabled={!selected.length}>Deactivate</button
+			>
 		</div>
+		<p class="text-sm">
+			Deactivation is not destructive: the creator(s) will still have access to the feedbacks, but
+			the entry will not receive any new votes
+		</p>
 	</form>
 </article>
 
