c74f7a4983354830262ceaf89ed521616e81d057
fcrozatier <frederic.crozatier@protonmail.com>
2024-08-01 10:44:41 +0200

    carousel component

---
+++ src/lib/components/Carousel.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import type { SelectEntry } from '$lib/server/db/schema';
+	import Youtube from './Youtube.svelte';
+
+	export let entries: Pick<SelectEntry, 'category' | 'url' | 'thumbnail'>[];
+</script>
+
+<div
+	class="scrollbar mx-auto flex max-w-5xl snap-x snap-proximity snap-always items-center gap-10 overflow-x-scroll pb-2"
+	style:--scrollbar-thumb="var(--light-gold)"
+>
+	{#each entries as entry}
+		<div class="snap-center">
+			{#if entry.category === 'video'}
+				<Youtube src={entry.url}></Youtube>
+			{:else}
+				<a href={entry.url} target="_blank" class="inline-block w-[420px]">
+					<img
+						src={entry.thumbnail}
+						alt="Winner thumbnail"
+						width="420"
+						class="aspect-video rounded-lg"
+						loading="lazy"
+					/>
+				</a>
+			{/if}
+		</div>
+	{/each}
+</div>
