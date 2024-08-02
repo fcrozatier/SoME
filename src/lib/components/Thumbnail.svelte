13f1097f14e6c34f4fbcce6a5d9b31fc34f825a0
fcrozatier <frederic.crozatier@protonmail.com>
2024-08-02 10:10:22 +0200

    lazy load thumbnail

--- src/lib/components/Thumbnail.svelte
+++ src/lib/components/Thumbnail.svelte
@@ -9,5 +9,6 @@
 	class="my-0 max-w-full rounded-lg aspect-video"
 	src={`https://${PUBLIC_S3_BUCKET}.${PUBLIC_S3_ENDPOINT.replace('https://', '')}/${uid}`}
 	alt="thumbnail"
+	loading="lazy"
 	{width}
 />
