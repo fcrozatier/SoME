84c3a30e88a3351db2bb57db84e36f26dd42a552
fcrozatier <frederic.crozatier@protonmail.com>
2024-08-08 10:03:41 +0200

    generic fixtures

--- src/routes/admin/fixtures/+page.server.ts
+++ src/routes/admin/fixtures/+page.server.ts
@@ -1,33 +1,40 @@
 import { db } from '$lib/server/db/client';
 import { entries } from '$lib/server/db/schema';
 import { fail } from '@sveltejs/kit';
+import { non_videos as n2021 } from './2021_non_videos';
 import { videos as v2021 } from './2021_videos';
+import { non_videos as n2022 } from './2022_non_videos';
 import { videos as v2022 } from './2022_videos';
 import { videos as v2023 } from './2023_videos';
 
 const data = {
-	'2023': v2023,
-	'2022': v2022,
-	'2021': v2021,
+	v2023: v2023,
+	v2022: v2022,
+	v2021: v2021,
+	n2021: n2021,
+	n2022: n2022,
 };
 
 export const actions = {
 	default: async ({ request }) => {
-		const year = (await request.formData()).get('year')?.toString();
-		if (!year) return fail(400);
-		const videos = data[year as keyof typeof data];
+		const type = (await request.formData()).get('type')?.toString();
+		if (!type) return fail(400);
+		const category = type[0] === 'v' ? 'video' : 'non-video';
+		const year = type.slice(1);
 
-		for (let i = 0; i < videos.length; i++) {
+		const all = data[type as keyof typeof data];
+
+		for (let i = 0; i < all.length; i++) {
 			await db.insert(entries).values({
-				category: 'video',
-				title: videos[i].title,
-				url: videos[i].href,
+				category,
+				title: all[i]?.title ?? '',
+				url: all[i].url,
 				rank: i + 1,
 				description: '',
 				uid: crypto.randomUUID(),
 				createdAt: `${year}-06-01`,
 			});
 		}
-		console.log(`added ${year} videos`);
+		console.log(`added ${year} ${category}s`);
 	},
 };
--- src/routes/admin/fixtures/+page.svelte
+++ src/routes/admin/fixtures/+page.svelte
@@ -1,7 +1,13 @@
 <article class="prose mx-auto">
 	<form method="post">
-		<button class="btn" name="year" value="2023">Add 2023 videos</button>
-		<button class="btn" name="year" value="2022">Add 2022 videos</button>
-		<button class="btn" name="year" value="2021">Add 2021 videos</button>
+		<div class="flex gap-4">
+			<button class="btn" name="type" value="v2023">Add 2023 videos</button>
+			<button class="btn" name="type" value="v2022">Add 2022 videos</button>
+			<button class="btn" name="type" value="v2021">Add 2021 videos</button>
+		</div>
+		<div class="flex gap-4 mt-4">
+			<button class="btn" name="type" value="n2022">Add 2022 non-videos</button>
+			<button class="btn" name="type" value="n2021">Add 2021 non-videos</button>
+		</div>
 	</form>
 </article>
