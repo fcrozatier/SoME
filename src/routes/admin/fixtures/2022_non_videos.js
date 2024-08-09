65345dc9c028d65849003730946bc8b2ea650f41
fcrozatier <frederic.crozatier@protonmail.com>
2024-08-09 11:21:15 +0200

    grab data

--- src/routes/admin/fixtures/2022_non_videos.js
+++ src/routes/admin/fixtures/2022_non_videos.js
@@ -1,389 +1,319 @@
-export const non_videos = [
-	{
-		url: 'https://www.lukelavalva.com/theoryofsliding',
-	},
+[
+	{ url: 'https://www.lukelavalva.com/theoryofsliding', title: 'Theory of Sliding' },
 	{
 		url: 'https://explanaria.github.io/crystalgroups',
+		title: 'Groups & Clear Crystal Conundrums',
 	},
 	{
 		url: 'https://www.ethanepperly.com/index.php/2022/08/13/chebyshev-polynomials/',
+		title: 'Chebyshev Polynomials',
 	},
 	{
 		url: 'https://calmcode.io/blog/inverse-turing-test.html',
+		title: 'Inverse Turing Test',
 	},
 	{
 		url: 'http://xperimex.com/blog/panorama-homography/',
+		title: 'Pleasing Panoramas and Matrix Multiplication',
 	},
 	{
 		url: 'https://btm.qva.mybluehost.me/solving-freudenthals-impossible-problem/',
+		title: 'Solving Freudenthal’s Impossible Problem',
 	},
 	{
 		url: 'https://www.summbit.com/blog/bezier-curve-guide/',
+		title: 'An Interactive Guide for Bézier Curves',
 	},
 	{
 		url: 'https://allroadslead2rome.vercel.app/',
+		title: 'All roads lead to Rome, a colorful problem',
 	},
 	{
 		url: 'https://colab.research.google.com/drive/1LyZ6br5LUvOVFqZIbYjQr7v5XIx98w5a?usp=sharing',
+		title: 'The Hot Chocolate Effect',
 	},
-	{
-		url: 'https://sasha-d.itch.io/primes',
-	},
+	{ url: 'https://sasha-d.itch.io/primes', title: 'Gaussian primes' },
 	{
 		url: 'https://voxelrifts.itch.io/graphit',
+		title: 'GraphIt',
 	},
 	{
 		url: 'http://mathstoshare.com/2022/06/23/a-clock-is-a-one-dimensional-subgroup-of-the-torus/',
+		title: 'A clock is a one-dimensional subgroup of the torus',
 	},
 	{
 		url: 'http://sphereplane.space/a-sphere-and-a-plane',
+		title: 'A Sphere, A Plane, and the Early Solar System',
 	},
 	{
 		url: 'https://www.alanzucconi.com/?p=8799',
+		title: 'The Mathematics of the Kalman Filter',
 	},
 	{
 		url: 'http://sambrunacini.com/computational-geometry-and-topographical-maps/',
+		title: 'Computational Geometry and Topographical Maps',
 	},
 	{
 		url: 'https://almostsurelymath.blog/2022/07/28/does-every-game-have-a-winner/',
+		title: 'Does Every Game have a Winner?',
 	},
-	{
-		url: 'https://fourierplayground.esdy.io/',
-	},
+	{ url: 'https://fourierplayground.esdy.io/', title: 'Fourier Playground' },
 	{
 		url: 'https://quantum9innovation.github.io/axiom/posts/pythagorean/',
+		title: 'Finding Every Possible Pythagorean Triple',
 	},
 	{
 		url: 'https://viola-buddy.itch.io/clock-solitaire',
+		title: 'Clock Solitaire (and Mathematical Analysis)',
 	},
-	{
-		url: 'https://chessengines.org/',
-	},
+	{ url: 'https://chessengines.org/', title: 'Chess Engines: A Zero to One Guide' },
 	{
 		url: 'https://blog.sheddow.xyz/gcd-euclid-and-bezout/',
+		title: 'GCD, Euclid and Bezout: an Eternal Golden Braid',
 	},
 	{
 		url: 'https://thenumb.at/Autodiff/',
+		title: 'Differentiable Programming from Scratch',
 	},
 	{
 		url: 'https://www.theadder.org/computational-complexity-explained-through-songs/',
+		title: 'Computational Complexity, Explained Through Songs',
 	},
 	{
 		url: 'https://www.stemwikitextbook.com/w/Volume_of_a_Pyramid',
+		title: 'Volume of a Pyramid',
 	},
-	{
-		url: 'http://indriid.com/2022/2022-08-13-summary-xia.pdf',
-	},
+	{ url: 'http://indriid.com/2022/2022-08-13-summary-xia.pdf', title: 'Growing Leaves' },
 	{
 		url: 'https://www.preethamrn.com/posts/fast-inverse-sqrt/',
+		title: 'How Fast Inverse Square Root actually works',
 	},
 	{
 		url: 'https://medium.com/@zmanswimmer/in-defense-of-approximations-6133408feda2',
+		title: 'In Defense of Approximations.',
 	},
-	{
-		url: 'https://vectorfield-dot-starfree.ew.r.appspot.com/',
-	},
+	{ url: 'https://vectorfield-dot-starfree.ew.r.appspot.com/', title: 'Vector Field' },
 	{
 		url: 'https://henrycoyledm.github.io/',
+		title: "Cayley's Theorem and Group Representations",
 	},
 	{
 		url: 'https://negativeonetwelfth.wordpress.com/2022/08/04/so-whats-up-with-those-mathematicians/',
-	},
-	{
-		url: 'http://soti.camp/blog_posts/imo_1988_problem_6.html',
+		title: 'So what’s up with those mathematicians?',
 	},
 	{
 		url: 'https://j-t-cano.tumblr.com/some2022',
+		title: 'The Transverse Asymmetry of a Glass of Water',
 	},
 	{
 		url: 'https://veryunknown.com/post/lambda-calculus-visualized/',
+		title: 'Understanding Computing Through Visualized Lambda Calculus',
 	},
 	{
 		url: 'https://phdenzel.github.io/2022/08/11/einstein-fake-proof/',
+		title: 'A Tale of Triangles',
 	},
-	{
-		url: 'https://www.dspforaudioprogramming.com/',
-	},
-	{
-		url: 'https://endremborza.github.io/funtum-computing/',
-	},
-	{
-		url: 'https://visualize-it.github.io/',
-	},
-	{
-		url: 'https://tiusic.com/quasi_crystals.html',
-	},
+	{ url: 'https://www.dspforaudioprogramming.com/', title: 'Audio Programming Tutorial' },
+	{ url: 'https://endremborza.github.io/funtum-computing/', title: 'Quantum Tinkering' },
+	{ url: 'https://visualize-it.github.io/', title: 'Visualize It' },
+	{ url: 'https://tiusic.com/quasi_crystals.html', title: 'Quasicrystals' },
 	{
 		url: 'https://medium.com/@lextmcdowell/factoring-with-geometry-lestras-method-e34f917faf4',
+		title: 'Factoring With Geometry: Lenstra’s Method',
 	},
 	{
 		url: 'https://lumoslabs.itch.io/lumos-quantum-laboratory',
+		title: 'Lumos Quantum Laboratory by lumoslabs',
 	},
-	{
-		url: 'https://beanway.me/nap/',
-	},
-	{
-		url: 'https://complex-analysis.com/',
-	},
+	{ url: 'https://beanway.me/nap/', title: 'NAP — A Game About Symmetry' },
 	{
 		url: 'https://dtcupcakes.github.io/maths-laboratory/',
+		title: 'Why Gaussian Processes are the Most Flexible Model',
 	},
 	{
 		url: 'https://mohamedrezk122.github.io/diffusion',
+		title: 'Diffusion Equation: a computational approach',
 	},
 	{
 		url: 'https://typical-teacher-cff.notion.site/a-journey-on-a-map-of-shapes-25e5c9a8df54409586536637d53138bf',
+		title: 'A journey on a map of shapes',
 	},
 	{
 		url: 'https://www.ddanieltan.com/posts/some2/',
+		title: 'Explainers: Inverse Transform Theorem',
 	},
-	{
-		url: 'https://maggieliu.dev/posts/zk',
-	},
+	{ url: 'https://maggieliu.dev/posts/zk', title: 'Zero-knowledge Proofs' },
 	{
 		url: 'https://medium.com/@masonprice_50176/non-elementary-integrals-approximating-the-impossible-2a98700bce2d',
+		title: 'Non-Elementary Integrals: approximating an impossible function',
 	},
 	{
 		url: 'https://coal-sneezeweed-6bd.notion.site/Introducing-the-Mathematics-of-the-Network-Effect-35b572086ae14e65bcf4c5131a1954d5',
+		title: 'Introducing the Mathematics of the Network Effect',
 	},
 	{
 		url: 'https://sites.google.com/view/stablejess/ultraproducts',
+		title: 'All About Ultraproducts',
 	},
 	{
 		url: 'https://gustavo-sopena.github.io/projects/Theorem-Hunt-SoME2',
+		title: 'Theorem Hunt',
 	},
 	{
 		url: 'https://github.com/UraniumCronorum/Summer-of-Math-Exposition/blob/main/SoME2/Complex_Exponents-final.pdf',
+		title: 'Complex Exponents',
 	},
 	{
 		url: 'https://thebees2012.files.wordpress.com/2022/08/flipbook.pdf',
+		title: 'Emma and the Oldest Magic',
 	},
-	{
-		url: 'https://contort-plotter.pages.dev/',
-	},
-	{
-		url: 'https://omegasd18.github.io/posts/RubiksGroup/',
-	},
+	{ url: 'https://contort-plotter.pages.dev/', title: 'Contort' },
+
 	{
 		url: 'https://ebanflo42.github.io/resources/docs/blog/geometryOfZipf.html',
+		title: "The Geometry of Zipf's Law",
 	},
 	{
 		url: 'https://neizod.dev/2022/06/10/sum-of-powers.html',
+		title: 'Sum of powers',
 	},
 	{
 		url: 'https://alephoneplex.com/2022/08/16/schroders-equation/',
+		title: 'Schröder’s Equation',
 	},
 	{
 		url: 'https://github.com/Wow25/GraphJumper',
-	},
-	{
-		url: 'https://sites.google.com/d/1_jcTUJODGejleGdgvBufsno-7XbnoQrI/p/14ZdWz_L54b6eax_auBNdVuz9Tk3jnwK3/edit',
-	},
-	{
-		url: 'https://linalg.dev/',
+		title: 'Graph Jumper',
 	},
 	{
 		url: 'https://recktenwald.github.io/SOME2',
+		title: 'Constructing a Pentagon from First Principles',
 	},
-	{
-		url: 'https://intothemorphism.com/2022/07/21/what-is-homeomorphism/#more-57',
-	},
+
 	{
 		url: 'https://rcerc.github.io/posts/making-lemonade-and-modular-inverses',
+		title: 'Making Lemonade and Modular Inverses',
 	},
-	{
-		url: 'https://thelongline.notion.site/Semantics-of-numbers-2a1f10c731b94abc8207eadbb8a21fa0',
-	},
+
 	{
 		url: 'https://a-j-smith.github.io/adams-website',
+		title: 'Tangent circles, Curvature and Parallel paths',
 	},
-	{
-		url: 'https://1drv.ms/b/s!AqUHOZl0LXnHjNYFPgrmAlt7DdZVxg?e=FqxaCg',
-	},
-	{
-		url: 'https://tarpon-grasshopper-3fsf.squarespace.com/',
-	},
-	{
-		url: 'https://mwageringel.github.io/everest/',
-	},
-	{
-		url: 'https://some2.timotheehuneau.fr/',
-	},
+
+	{ url: 'https://mwageringel.github.io/everest/', title: 'Everest' },
+	{ url: 'https://some2.timotheehuneau.fr/', title: 'Extraordinary ordinals' },
 	{
 		url: 'https://engitom.github.io/math/teaching/summer-of-math-exposition-2/',
+		title: 'Golden Snake',
 	},
-	{
-		url: 'https://davidtranhq.github.io/mitras/',
-	},
-	{
-		url: 'https://quantiset.itch.io/vector-space',
-	},
+
+	{ url: 'https://quantiset.itch.io/vector-space', title: 'Vector Space by Quantiset' },
 	{
 		url: 'https://www.sunclipse.org/?p=3110',
+		title: 'A Picture for the Mind: the Bloch Ball',
 	},
 	{
 		url: 'https://www.s-tronomic.in/post/103',
+		title: 'How Adding a New Expressway Might Lead To More Traffic?',
 	},
-	{
-		url: 'https://aissakhanov.pythonanywhere.com/',
-	},
-	{
-		url: 'https://bulmenisaurus.github.io/SoME2/',
-	},
+	{ url: 'https://bulmenisaurus.github.io/SoME2/', title: 'Counting set bits' },
 	{
 		url: 'https://medium.com/@patrickmartinaz/please-stop-talking-about-the-central-limit-theorem-4f727021e1c6',
+		title: 'Please Stop Talking About the Central Limit Theorem',
 	},
 	{
 		url: 'https://journal.medicine.berlinexchange.de/pub/hypotest/release/1',
-	},
-	{
-		url: 'https://clement-lelievre-riemann-rearrangement-some2022-main-wzd254.streamlitapp.com/',
+		title:
+			"Inferential Statistics I: Hypothesis Testing in the Basic Form of Conditional Probability / Bayes' Rule",
 	},
 	{
 		url: 'https://ryandcole.eu5.org/iblog/szeta.html',
+		title: 'Fractal Strings and the Riemann Zeta Function',
 	},
 	{
 		url: 'https://ccbotix.itch.io/special-relativity-sandbox',
+		title: 'Special Relativity Sandbox',
 	},
 	{
 		url: 'https://www.algolia.com/blog/engineering/algolias-compression-algorithm-inspired-by-lightning-and-coin-sorters/',
+		title: 'Learn how Algolia’s compression algorithm is inspired by lightning and coin sorters',
 	},
 	{
 		url: 'https://aravindchandradoss.github.io/blog/blog_pca.html',
+		title: 'Principal Component Analysis (PCA) and why should we know them',
 	},
-	{
-		url: 'http://www.graphwar.com/tutorial.html',
-	},
-	{
-		url: 'https://musescore.com/user/37605066/sheetmusic?page=8&sort=date_uploaded',
-	},
+	{ url: 'http://www.graphwar.com/tutorial.html', title: 'Graphwar Tutorial' },
 	{
 		url: 'https://edgesandnodes.itch.io/edges-and-nodes',
+		title: 'Edges and Nodes',
 	},
 	{
 		url: 'https://drive.google.com/drive/folders/1tuGZ7CP9QVAgDw7ryp-Nx2_d971BBfjl?usp=sharing',
+		title: 'Mathematical Conlangery',
 	},
 	{
 		url: 'https://docs.google.com/document/d/18KPvBZtvcdO3Twr69m7sssO9p2SD0ip9bjkb-KwmGlc/edit?usp=sharing',
-	},
-	{
-		url: 'https://pollorollo.github.io/writing/emptySet',
-	},
-	{
-		url: 'https://jackli777.github.io/3b1b-some2-2022-/',
+		title: "The Enumeration of Possible Scrambles on a 3x3 Rubik's Cube",
 	},
 	{
 		url: 'https://sites.google.com/view/whats-so-special-about-pascals/home',
-	},
-	{
-		url: 'https://www.daniel-scott-carter.com/calculus-made-easy-differentiation/',
+		title: "What's So Special About Pascal's Triangle?",
 	},
 	{
 		url: 'https://subde.pages.iu.edu/Blog/inverse%20square%20from%20photon.htm',
-	},
-	{
-		url: 'https://pmanot.github.io/post/essence.html',
+		title: 'From two perfect mirrors and a photon',
 	},
 	{
 		url: 'https://www.scribd.com/document/586870870/The-Geometric-Series-Unit-Circle',
-	},
-	{
-		url: 'https://t.me/riemann_hypothesis_816_xscape_rm',
+		title: 'The Geometric Series Unit Circle',
 	},
 	{
 		url: 'https://sites.google.com/view/diophantine-equations-some2/home/',
-	},
-	{
-		url: 'https://twitter.com/dcwych/status/1541910248574791680?s=20&t=zBo2th1RiGOXVLlB6CT1kQ',
-	},
-	{
-		url: 'https://rebrand.ly/ef056d',
+		title: 'Diophantine Equations',
 	},
 	{
 		url: 'https://docs.google.com/document/d/1l647J5KoOUnUeL6DCl6wATWl-_sp4TTfhNynuFgPc1A/edit?usp=sharing',
-	},
-	{
-		url: 'https://jeff-suliga.github.io/SoME2',
+		title: 'Looking at a Ring',
 	},
 	{
 		url: 'https://at-290690.github.io/SoME2-Entry-CT/',
+		title: 'Category Theory Playground',
 	},
-	{
-		url: 'https://qr.ae/pvPess',
-	},
-	{
-		url: 'https://www.conwaygameoflife.net/',
-	},
-	{
-		url: 'https://www.tristansonlinephysicsnotes.com/',
-	},
-	{
-		url: 'https://winstondegreef.com/what-pi-looks-like',
-	},
+
 	{
 		url: 'https://mathsfromnothing.cf/partial-fractions/',
-	},
-	{
-		url: 'https://imakappa.github.io/geomalgebra/',
+		title: 'Partial Fractions',
 	},
 	{
 		url: 'https://docs.google.com/presentation/d/15tlHZGAiPnDHujrvM1COYg5HbfjmNw-uNFmDADNdrvs/edit?usp=sharing',
+		title: 'Cryptography, the Eighth Wonder of the World',
 	},
-	{
-		url: 'https://dynamicalsystems.com.au/',
-	},
-	{
-		url: 'https://www.lefthandedfrog.co.uk/',
-	},
-	{
-		url: 'https://www.wolframcloud.com/obj/lucdouwen/Published/TheIntuitiveFundamentalTheoremOfCalculus-source.nb',
-	},
-	{
-		url: 'https://www.intelligencelectures.com/post/needs',
-	},
-	{
-		url: 'https://photos.app.goo.gl/8AVFQXH4KWQMg7ip9',
-	},
+
 	{
 		url: 'https://gmdq.substack.com/p/knapsack-problem',
+		title: 'Knapsack Problem',
 	},
-	{
-		url: 'https://www.empamathic.com/mathmerize.html',
-	},
+	{ url: 'https://www.empamathic.com/mathmerize.html', title: 'Mathmerize' },
 	{
 		url: 'https://mathematics877465002.wordpress.com/?p=8',
+		title: 'Extending the Concept of Continuity of real functions.',
 	},
+
 	{
-		url: 'https://1drv.ms/w/s!ApOz05bPw2kRjFLiawOWMVCo2VF_',
-	},
-	{
-		url: 'https://dachshundstudios.itch.io/math-puzzle-1/devlog/416013/some-2-submission',
-	},
-	{
-		url: 'https://playfulmathematician.github.io/PlayfulMathematicianArticles/coinspt1',
+		url: 'https://www.playfulmathematician.com/coinspt1.html',
+		title: 'A Really Specific Puzzle On Coin Flipping Part 1',
 	},
 	{
 		url: 'https://humourprince.wordpress.com/2022/08/06/expressing-integers-as-a-sum-or-difference-of-powers/',
+		title: 'Expressing integers as a Sum or Difference of powers',
 	},
 	{
 		url: 'https://www.academia.edu/84245507/An_Essay_on_how_Quanta_Affect_the_Math_behind_the_Universe',
+		title: 'An Essay on how Quanta Affect the Math behind the Universe',
 	},
-	{
-		url: 'https://stemandmusic.in/',
-	},
+	{ url: 'https://stemandmusic.in/', title: 'STEM and Music' },
 	{
 		url: 'https://www.academia.edu/83250455/The_Theory_of_Dartions',
-	},
-	{
-		url: 'https://twitter.com/matheverydayy/status/1558931204673503240?t=hDCq8LITA8m_lZXo1obm-A&s=19',
-	},
-	{
-		url: 'https://www.instagram.com/teaformulamaths/',
-	},
-	{
-		url: 'https://darkcheftar.github.io/math/3xp1/',
-	},
-	{
-		url: 'https://drive.google.com/drive/folders/1ATw2d5VGyXShx0yPrFAc9Mgca4vx7LFs?usp=sharing',
+		title: 'The Theory of Dartions',
 	},
 ];
