90a6a24427f8b6f8e33b23100c8ff6d724389782
fcrozatier <frederic.crozatier@protonmail.com>
2024-09-04 11:14:13 +0200

    redirect with correct params

--- src/routes/non-videos/+page.server.ts
+++ src/routes/non-videos/+page.server.ts
@@ -2,5 +2,5 @@ import { redirect } from '@sveltejs/kit';
 
 export const load = async () => {
 	console.log('REDIRECTING non-videos -> archive');
-	redirect(301, '/archive');
+	return redirect(301, '/archive?category=non-video&year=2023&page=1');
 };
