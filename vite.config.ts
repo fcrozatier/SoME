d4cbe3276f3aaddf9a42dbc908c7d30488eb6225
fcrozatier <frederic.crozatier@protonmail.com>
2025-03-03 10:22:18 +0100

    install tailwind 4 and daisy 5

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ["src/**/*.{test,spec}.{js,ts}"],
-	},
+	plugins: [sveltekit(), tailwindcss()],
 	ssr: {
 		noExternal: ["formgator"],
 	},
