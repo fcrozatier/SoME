9d5dfc391c978142e789b9d9ed7ced40088cde43
fcrozatier <frederic.crozatier@protonmail.com>
2025-03-13 11:49:20 +0100

    use lite-yt

--- src/lib/components/Youtube.svelte
+++ src/lib/components/Youtube.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
 	interface Props {
 		src: string;
-		width?: number;
+		title?: string;
 	}
 
-	let { src, width = 420 }: Props = $props();
+	let { src, title }: Props = $props();
+
+	let videoid = src.split("/").at(-1);
 </script>
 
-<iframe
+<!-- TODO remove -->
+<!-- <iframe
 	{width}
 	class="aspect-video rounded-lg"
 	{src}
@@ -17,4 +20,42 @@
 	referrerpolicy="strict-origin-when-cross-origin"
 	allowfullscreen
 	loading="lazy"
-></iframe>
+></iframe> -->
+
+<lite-youtube {videoid} videotitle={title} autoload>
+	<a class="lite-youtube-fallback" href={`https://www.youtube.com/watch?v=${videoid}`}>
+		Watch on YouTube: {title}
+	</a>
+</lite-youtube>
+
+<style>
+	.lite-youtube-fallback {
+		aspect-ratio: 16 / 9; /* matches YouTube player */
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		gap: 1em;
+		padding: 1em;
+		background-color: #000;
+		color: #fff;
+		text-decoration: none;
+	}
+
+	/* right-facing triangle "Play" icon */
+	.lite-youtube-fallback::before {
+		display: block;
+		content: "";
+		border: solid transparent;
+		border-width: 2em 0 2em 3em;
+		border-left-color: red;
+	}
+
+	.lite-youtube-fallback:hover::before {
+		border-left-color: #fff;
+	}
+
+	.lite-youtube-fallback:focus {
+		outline: 2px solid red;
+	}
+</style>
