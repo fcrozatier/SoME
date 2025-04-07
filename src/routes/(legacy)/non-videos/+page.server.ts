ca6615a58ca4d508ecea69e9afb812b6f0b3c760
fcrozatier <frederic.crozatier@protonmail.com>
2025-04-07 14:52:01 +0200

    change log

--- src/routes/(legacy)/non-videos/+page.server.ts
+++ src/routes/(legacy)/non-videos/+page.server.ts
@@ -1,6 +1,6 @@
 import { redirect } from "@sveltejs/kit";
 
 export const load = async ({ request }) => {
-	console.log("REDIRECTING non-videos -> archive", request.referrer);
+	console.log("[LEGACY]: non-videos -> archive", request.referrer);
 	redirect(301, "/archive?category=non-video&year=2023&page=1");
 };
--- src/routes/(legacy)/previous/+page.server.ts
+++ src/routes/(legacy)/previous/+page.server.ts
@@ -1,6 +1,6 @@
 import { redirect } from "@sveltejs/kit";
 
 export const load = ({ request }) => {
-	console.log("REDIRECTING previous -> archive", request.referrer);
+	console.log("[LEGACY] previous -> archive", request.referrer);
 	redirect(301, "/archive");
 };
