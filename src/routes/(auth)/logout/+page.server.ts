dd15f44b2bd0f767b9e3f7999351a45a9ac55272
fcrozatier <frederic.crozatier@protonmail.com>
2025-04-07 17:53:48 +0200

    add logout endpoint

---
+++ src/routes/(auth)/logout/+page.server.ts
@@ -0,0 +1,23 @@
+import * as auth from "$lib/server/auth";
+import { fail, redirect } from "@sveltejs/kit";
+import type { Actions, PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async (event) => {
+	if (!event.locals.user) {
+		return redirect(302, "/");
+	}
+
+	if (!event.locals.session) {
+		return fail(401);
+	}
+
+	await auth.invalidateSession(event.locals.session.id);
+	auth.deleteSessionTokenCookie(event);
+
+	return redirect(303, "/");
+};
+
+export const actions: Actions = {
+	logout: async (event) => {
+	},
+};
