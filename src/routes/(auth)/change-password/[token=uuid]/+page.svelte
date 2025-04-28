36e8e5d51b8a11a122d20d61bf6bc76517d5c1dc
fcrozatier <frederic.crozatier@protonmail.com>
2025-04-28 13:53:22 +0200

    fix wording

--- src/routes/(auth)/change-password/[token=uuid]/+page.svelte
+++ src/routes/(auth)/change-password/[token=uuid]/+page.svelte
@@ -6,7 +6,7 @@
 
 <article class="layout-prose">
 	<h2>Password successfully reset!</h2>
-	<p>You can now login with your new password</p>
+	<p>You can now log in with your new password</p>
 	<p>
 		<a href={"/login"}>Login</a>
 	</p>
