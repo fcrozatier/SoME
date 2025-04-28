f8d2bd36d245656da5c08cbfdc81291d555ebd7f
fcrozatier <frederic.crozatier@protonmail.com>
2025-04-28 12:34:04 +0200

    update contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -8,8 +8,10 @@
 	<h2>Contact</h2>
 
 	<p>
-		This is a community-driven event, if you have questions or suggestions feel free to reach out
-		directly on <a href="https://discord.gg/WZvZMVsXXR">Discord</a> or by email at
+		If you have questions or suggestions feel free to reach out on <a
+			href="https://discord.gg/WZvZMVsXXR">Discord</a
+		>
+		or by email at
 		<a href="mailto:3b1b.some@gmail.com"> 3b1b.some@gmail.com</a>
 	</p>
 </article>
