dd2800ff58a1d634c192c26564713d11004c2439
fcrozatier <frederic.crozatier@protonmail.com>
2025-05-05 13:41:45 +0200

    add a profile page

---
+++ src/routes/user/profile/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<article class="layout-prose">
+	<h2>Profile</h2>
+	<p>
+		<a href="/logout" class="btn" data-sveltekit-preload-data="off" data-sveltekit-reload
+			>Sign out</a
+		>
+	</p>
+</article>
