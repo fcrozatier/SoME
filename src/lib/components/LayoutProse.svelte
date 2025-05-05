f89d094e3da3b365aa6388234cbf3e00b1acffcc
fcrozatier <frederic.crozatier@protonmail.com>
2025-05-05 12:03:45 +0200

    fix md layouts

--- src/lib/components/LayoutProse.svelte
+++ src/lib/components/LayoutProse.svelte
@@ -8,7 +8,6 @@
 	let { children }: Props = $props();
 </script>
 
-<!-- Component for the Content Policy -->
 <article class="prose max-w-4xl mx-auto px-8">
 	{@render children?.()}
 </article>
--- svelte.config.js
+++ svelte.config.js
@@ -2,24 +2,21 @@ import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { mdsvex } from "mdsvex";
 import rehypeTargetBlank from "rehype-target-plugin";
-import targetBlank from "svelte-target-blank";
 
 /**
  * @type {import('mdsvex').MdsvexOptions}
  */
 const mdsvexOptions = {
 	extensions: [".md"],
-	layout: "./src/lib/components/LayoutProse.svelte",
 	rehypePlugins: [rehypeTargetBlank],
 };
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: [".svelte", ".svx", ".md"],
+	extensions: [".svelte", ".md"],
 	preprocess: [
-		vitePreprocess(),
 		mdsvex(mdsvexOptions),
-		targetBlank({ mode: "warn", silentList: "/**/*.md" }),
+		vitePreprocess(),
 	],
 
 	kit: {
