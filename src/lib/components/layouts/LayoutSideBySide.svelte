02e7b89aa77ca4d3c67b0bf692493a010398f1a2
fcrozatier <frederic.crozatier@protonmail.com>
2025-05-12 17:51:40 +0200

    improve the side-by-side layout

--- src/lib/components/layouts/LayoutSideBySide.svelte
+++ src/lib/components/layouts/LayoutSideBySide.svelte
@@ -2,16 +2,56 @@
 	import type { Snippet } from "svelte";
 
 	interface Props {
+		/**
+		 * The side is the column of fixed width
+		 */
+		side: "left" | "right";
+		/**
+		 * flex-basis width of the side
+		 */
 		sideWidth?: string;
+		/**
+		 * content min-inline-size as a percent of the parent containing both the side and the main content
+		 */
 		contentMinSize: `${number}%`;
+		/**
+		 * number on the --spacing scale
+		 *
+		 * gap: 4 corresponds to calc(var(--spacing) * 4)
+		 */
+		gap?: number;
+		/**
+		 * How items are aligned
+		 *
+		 * @default flex-start
+		 */
+		alignment?: "flex-start" | "center" | "flex-end";
+		/**
+		 * The `class` attribute of the container
+		 */
+		class?: string;
 		sidePanel: Snippet;
 		mainPanel: Snippet;
 	}
 
-	let { sideWidth, contentMinSize = "50%", sidePanel, mainPanel }: Props = $props();
+	let {
+		side = "left",
+		gap = 4,
+		sideWidth,
+		contentMinSize = "50%",
+		class: className = "",
+		alignment = "flex-start",
+		sidePanel,
+		mainPanel,
+	}: Props = $props();
 </script>
 
-<div data-layout="side-by-side">
+<div
+	class={className}
+	class:reverse={side === "right"}
+	data-layout="side-by-side"
+	style={`--gap: calc(var(--spacing) * ${gap}); align-items: ${alignment}`}
+>
 	<div style={sideWidth ? `flex-basis: ${sideWidth}` : ""}>{@render sidePanel()}</div>
 	<div style={`min-inline-size: ${contentMinSize}`}>{@render mainPanel()}</div>
 </div>
@@ -20,8 +60,7 @@
 	[data-layout="side-by-side"] {
 		display: flex;
 		flex-wrap: wrap;
-		gap: calc(var(--spacing) * 4);
-		align-items: flex-start;
+		gap: var(--gap);
 
 		& > :first-child {
 			flex-grow: 1;
@@ -30,6 +69,19 @@
 		& > :last-child {
 			flex-basis: 0;
 			flex-grow: 9999;
+		}
+
+		&.reverse {
+			flex-direction: row-reverse;
+
+			& > :last-child {
+				flex-grow: 1;
+			}
+
+			& > :first-child {
+				flex-basis: 0;
+				flex-grow: 9999;
+			}
 		}
 	}
 </style>
