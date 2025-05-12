f7b29836f868120f99854b6ffb796f01a5e4d93e
fcrozatier <frederic.crozatier@protonmail.com>
2025-05-12 13:48:00 +0200

    load user entries

---
+++ src/routes/user/entries/+page.server.ts
@@ -0,0 +1,21 @@
+import { db } from "$lib/server/db";
+import type { SelectEntry } from "$lib/server/db/schema.js";
+import { redirect } from "@sveltejs/kit";
+import { sql } from "drizzle-orm";
+
+export const load = async ({ locals }) => {
+  if (!locals.user) return redirect(302, "/login");
+
+  const userEntries: Pick<
+    SelectEntry,
+    "uid" | "title" | "description" | "category" | "thumbnail" | "url"
+  >[] = await db.execute(sql`
+      select uid, title, description, category, thumbnail, url, created_at from entries
+      inner join user_to_entry
+      on entries.uid=user_to_entry.entry_uid
+      where user_to_entry.user_uid=${locals.user.uid}
+      order by entries.created_at desc
+    `);
+
+  return { userEntries };
+};
--- src/routes/user/entries/+page.svelte
+++ src/routes/user/entries/+page.svelte
@@ -1,19 +1,70 @@
 <script lang="ts">
-	import { PUBLIC_REGISTRATION_START } from "$env/static/public";
-
+	import { PUBLIC_REGISTRATION_END, PUBLIC_REGISTRATION_START } from "$env/static/public";
+	import Thumbnail from "$lib/components/Thumbnail.svelte";
 	import Time from "$lib/components/Time.svelte";
+	import Youtube from "$lib/components/Youtube.svelte";
+	import { submissionsOpen, YOUTUBE_EMBED } from "$lib/utils";
+
+	const { data } = $props();
 </script>
 
 <article class="layout-prose">
 	<h2>My entries</h2>
-	<p>No entry submitted</p>
+
 	<p>
-		Submissions will open on <strong><Time datetime={PUBLIC_REGISTRATION_START} /></strong>.
+		{#if new Date() < new Date(PUBLIC_REGISTRATION_START)}
+			Submissions open on <strong><Time datetime={PUBLIC_REGISTRATION_START} /></strong>.
+		{:else if new Date() < new Date(PUBLIC_REGISTRATION_END)}
+			The submission deadline is <strong><Time datetime={PUBLIC_REGISTRATION_END} /></strong>.
+		{:else}
+			Submissions are closed
+		{/if}
 	</p>
 	<p>
-		<button class="btn btn-neutral" disabled>New entry</button>
+		<button class="btn btn-neutral" disabled={!submissionsOpen()}>New entry</button>
 	</p>
-	<!-- <p>
-		The submission deadline is <strong><Time datetime={PUBLIC_REGISTRATION_END} /></strong>.
-	</p> -->
+
+	{#if data.userEntries.length > 0}
+		<div class="overflow-x-scroll sm:px-4">
+			<table class="table min-w-xl max-w-3xl mx-auto">
+				<thead>
+					<tr>
+						<th>Entry</th>
+						<th>Title</th>
+						<th class="text-center">Rank</th>
+					</tr>
+				</thead>
+				<tbody>
+					{#each data.userEntries as { uid, title, description, category, thumbnail, url }}
+						<tr>
+							<td>
+								{#if category === "video" && url && YOUTUBE_EMBED.test(url)}
+									{#key url}
+										<Youtube src={url} {title}></Youtube>
+									{/key}
+								{:else if thumbnail && url && !YOUTUBE_EMBED.test(url)}
+									<a href={url} target="_blank">
+										<Thumbnail uid={thumbnail} width={256}></Thumbnail>
+									</a>
+								{:else}
+									<a href={url} class="line-clamp-1" target="_blank">{url}</a>
+								{/if}
+							</td>
+							<td>
+								<h3 class="text-base text-balance line-clamp-2 text-trim mt-0">{title}</h3>
+								{#if description}
+									<p class="line-clamp-3">{description}</p>
+								{/if}
+							</td>
+							<td class="items-center flex flex-col gap-4">
+								<a class="btn btn-sm" href={`/entries/${uid}`}> details </a>
+							</td>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		</div>
+	{:else}
+		<p>No entry submitted</p>
+	{/if}
 </article>
