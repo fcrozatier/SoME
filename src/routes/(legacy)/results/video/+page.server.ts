549b1f06ee473cc61df6f46a8da0ce080c623c5e
fcrozatier <frederic.crozatier@protonmail.com>
2025-05-28 08:37:49 +0200

    redirect the results page to the archive

---
+++ src/routes/(legacy)/results/video/+page.server.ts
@@ -0,0 +1,6 @@
+import { redirect } from "@sveltejs/kit";
+
+export const load = async ({ request }) => {
+  console.log("[LEGACY]: non-videos -> archive", request.referrer);
+  return redirect(301, "/archive?category=video&year=2024&page=1");
+};
