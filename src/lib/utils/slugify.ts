ac39a74f61ce1467a1cc8de9ed445bc2300c05fe
fcrozatier <frederic.crozatier@protonmail.com>
2025-05-30 09:51:05 +0200

    strip non-ascii characters

--- src/lib/utils/slugify.ts
+++ src/lib/utils/slugify.ts
@@ -5,15 +5,16 @@ export const wordSegmenter = new Intl.Segmenter("en", { granularity: "word" });
  */
 export const NON_ASCII = /\P{ASCII}/gu;
 export const NON_ASCII_LETTER = /[^a-z]/gu;
+export const NON_WORD = /[^\p{L}\p{M}\p{N}\-]+/gu;
 
 /**
  * Slugifies an input string
  *
  * @param input Input string to slugify
- * @param strip The character set to strip from the slug. By default non letter characters are stripped out
+ * @param strip The character set to strip from the slug.
+ * @default NON_ASCII
  */
-
-export const slugify = (input: string, strip: RegExp = NON_ASCII_LETTER) => {
+export const slugify = (input: string, strip: RegExp = NON_ASCII) => {
 	const normalized = input.trim().toLowerCase().normalize("NFKD");
 	const words: string[] = [];
 
@@ -22,5 +23,8 @@ export const slugify = (input: string, strip: RegExp = NON_ASCII_LETTER) => {
 		else if (s.segment.length) words.push("-");
 	}
 
-	return words.join("-").replaceAll(/-{2,}/g, "-").replaceAll(/^-|-$/g, "").normalize("NFC");
+	return words.join("-")
+		.replaceAll(/-{2,}/g, "-")
+		.replaceAll(/^-|-$/g, "")
+		.normalize("NFC");
 };
