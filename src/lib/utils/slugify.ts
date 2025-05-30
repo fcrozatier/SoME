69daedc54660af0ac8842470d1f94a1e292e425e
fcrozatier <frederic.crozatier@protonmail.com>
2025-05-30 12:05:18 +0200

    format

--- src/lib/utils/slugify.ts
+++ src/lib/utils/slugify.ts
@@ -23,8 +23,5 @@ export const slugify = (input: string, strip: RegExp = NON_ASCII) => {
 		else if (s.segment.length) words.push("-");
 	}
 
-	return words.join("-")
-		.replaceAll(/-{2,}/g, "-")
-		.replaceAll(/^-|-$/g, "")
-		.normalize("NFC");
+	return words.join("-").replaceAll(/-{2,}/g, "-").replaceAll(/^-|-$/g, "").normalize("NFC");
 };
--- src/routes/(legacy)/results/non-video/+page.server.ts
+++ src/routes/(legacy)/results/non-video/+page.server.ts
@@ -1,6 +1,6 @@
 import { redirect } from "@sveltejs/kit";
 
 export const load = async ({ request }) => {
-  console.log("[LEGACY]: non-videos -> archive", request.referrer);
-  return redirect(301, "/archive?category=non-video&year=2024&page=1");
+	console.log("[LEGACY]: non-videos -> archive", request.referrer);
+	return redirect(301, "/archive?category=non-video&year=2024&page=1");
 };
