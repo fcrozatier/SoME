3026657ebf6bd43b60fbd7a748db792a621cea9f
Frédéric Crozatier <48696601+fcrozatier@users.noreply.github.com>
2025-05-31 14:44:08 +0200

    fix: url-safe slugs (#28)

--- src/lib/utils/slugify.ts
+++ src/lib/utils/slugify.ts
@@ -8,13 +8,25 @@ export const NON_ASCII_LETTER = /[^a-z]/gu;
 export const NON_WORD = /[^\p{L}\p{M}\p{N}\-]+/gu;
 
 /**
+ * Only keep unreserved URL-safe ASCII characters and avoid "." (path traversal safe) / "_" / "~"
+ *
+ * @see https://datatracker.ietf.org/doc/html/rfc3986#section-2.3
+ */
+export const NON_ASCII_ALPHANUMERIC_OR_DASH = /[^a-zA-Z0-9\-]/gu;
+
+/**
  * Slugifies an input string
+ *
+ * Use the {@linkcode NON_ASCII_ALPHANUMERIC_OR_DASH} filter for URL-safe ascii slugs
  *
  * @param input Input string to slugify
  * @param strip The character set to strip from the slug.
- * @default NON_ASCII
+ * @default NON_ASCII_ALPHANUMERIC_OR_DASH
  */
-export const slugify = (input: string, strip: RegExp = NON_ASCII) => {
+export const slugify = (
+	input: string,
+	strip: RegExp = NON_ASCII_ALPHANUMERIC_OR_DASH,
+) => {
 	const normalized = input.trim().toLowerCase().normalize("NFKD");
 	const words: string[] = [];
 
@@ -23,5 +35,6 @@ export const slugify = (input: string, strip: RegExp = NON_ASCII) => {
 		else if (s.segment.length) words.push("-");
 	}
 
-	return words.join("-").replaceAll(/-{2,}/g, "-").replaceAll(/^-|-$/g, "").normalize("NFC");
+	return words.join("-").replaceAll(/-{2,}/g, "-").replaceAll(/^-|-$/g, "")
+		.normalize("NFC");
 };
