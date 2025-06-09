01c29fac753dea00350d36bcce7bf0bd7d0fbfb6
fcrozatier <frederic.crozatier@protonmail.com>
2025-06-09 10:53:53 +0200

    format

--- src/lib/utils/dictionary.server.ts
+++ src/lib/utils/dictionary.server.ts
@@ -1,10 +1,8 @@
 import pluralize from "pluralize-esm";
 import dictText from "./dictionary.txt?raw";
 
-export const dictionary = new Set(
-  dictText.split("\n").map((w) => w.toLowerCase()),
-);
+export const dictionary = new Set(dictText.split("\n").map((w) => w.toLowerCase()));
 
 for (const word of dictionary) {
-  dictionary.add(pluralize.plural(word));
+	dictionary.add(pluralize.plural(word));
 }
