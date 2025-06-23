aca42b0fa5081f935693f57ef9c983b868391da1
fcrozatier <frederic.crozatier@protonmail.com>
2025-06-23 08:19:04 +0200

    early return

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -8,7 +8,7 @@ process.on("sveltekit:shutdown", (reason) => {
 });
 
 // I'm a teapot
-const tea = /(\.php$|\.env|\.xml$|\.git|wordpress|wp-content|wp-admin|credentials)/;
+const tea = /(\.php$|\.env|\.xml$|\.git|wordpress|wp-content|wp-admin|credentials|config)/;
 
 export const handle = async function ({ event, resolve }) {
 	if (tea.test(event.request.url)) {
