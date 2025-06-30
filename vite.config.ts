ac2e6428dd2e967f80d768be5b3a3d6f825e20a5
fcrozatier <frederic.crozatier@protonmail.com>
2025-06-30 11:13:07 +0200

    add devtools-json

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
+import devtoolsJson from "vite-plugin-devtools-json";
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()],
-	ssr: {
-		noExternal: ["formgator"],
-	},
+	plugins: [sveltekit(), tailwindcss(), devtoolsJson()],
+	ssr: { noExternal: ["formgator"] },
 });
