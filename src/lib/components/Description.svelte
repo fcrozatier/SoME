e25840c3b10c38012c809ad8ea8f92a37c424f91
fcrozatier <frederic.crozatier@protonmail.com>
2025-07-28 16:16:20 +0200

    collapsible description

---
+++ src/lib/components/Description.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	let { content }: { content: string } = $props();
+
+	let open = $state(false);
+</script>
+
+<div class="pile relative mb-10">
+	<details bind:open>
+		<summary>{content}</summary>
+		<section>{content}</section>
+	</details>
+
+	<div class="relative mt-4 -bottom-12 self-end flex justify-between items-center">
+		<hr class="bg-gray-50 w-full" />
+		<button
+			class="btn btn-outline btn-sm px-10 -bottom-3 left-0 border-gray-300 border"
+			onclick={() => (open = !open)}>{open ? "Less..." : "More..."}</button
+		>
+		<hr class="bg-gray-50 font-semibold w-full" />
+	</div>
+</div>
+
+<style>
+	details {
+		border: none;
+		padding-block: 0;
+		margin-bottom: calc(var(--spacing) * 6);
+
+		& > summary::before,
+		& > summary::after {
+			display: none;
+		}
+	}
+
+	summary {
+		position: absolute;
+
+		display: -webkit-box;
+		-webkit-box-orient: vertical;
+		-webkit-line-clamp: 3;
+		line-clamp: 3;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		height: 3lh;
+
+		padding: 0;
+		font-size: var(--text-base);
+		font-weight: var(--font-weight-normal);
+		text-box-trim: none;
+
+		cursor: auto;
+
+		transition: opacity 250ms;
+	}
+
+	details:open summary {
+		opacity: 0;
+	}
+</style>
