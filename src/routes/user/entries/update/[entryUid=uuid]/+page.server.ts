8d48c403c09bec7257733fbc13d57229f682430d
fcrozatier <frederic.crozatier@protonmail.com>
2025-08-18 15:29:36 +0200

    validate creation date on updates

--- src/routes/user/entries/update/[entryUid=uuid]/+page.server.ts
+++ src/routes/user/entries/update/[entryUid=uuid]/+page.server.ts
@@ -93,9 +93,7 @@ export const actions = {
 				}
 
 				const html = await r.text();
-				const embeddedjson = html.match(
-					/ytInitialPlayerResponse\s*=\s*(\{.+?\});/,
-				);
+				const embeddedjson = html.match(/ytInitialPlayerResponse\s*=\s*(\{.+?\});/);
 				if (!embeddedjson) {
 					console.log("[new entry]: couldn't parse yt metadata from", data.url);
 				} else {
@@ -103,8 +101,7 @@ export const actions = {
 						const ytInitialPlayerResponse = embeddedjson[1]!;
 						// var channel = JSON.parse(ytInitialPlayerResponse).videoDetails.author;
 						var createdAt =
-							JSON.parse(ytInitialPlayerResponse).microformat
-								.playerMicroformatRenderer.uploadDate;
+							JSON.parse(ytInitialPlayerResponse).microformat.playerMicroformatRenderer.uploadDate;
 					} catch (error) {
 						console.log("[new entry]: error wile parsing yt metadata", error);
 					}
@@ -112,8 +109,7 @@ export const actions = {
 					// Check whether content is too old
 					if (new Date(createdAt) < new Date(PUBLIC_REGISTRATION_START)) {
 						return formfail({
-							url:
-								`This entry is too old to be eligible. Only recent work can be submitted for SoME. See the rules for more details`,
+							url: `This entry is too old to be eligible. Only recent work can be submitted for SoME. See the rules for more details`,
 						});
 					}
 				}
@@ -139,9 +135,7 @@ export const actions = {
 				.from(users)
 				.where(inArray(users.username, usernames));
 
-			const formerCoauthors = prevCoauthors.filter((a) =>
-				!usernames.includes(a.username!)
-			);
+			const formerCoauthors = prevCoauthors.filter((a) => !usernames.includes(a.username!));
 
 			// Validate team members
 			if (team.length !== usernames.length) {
@@ -172,9 +166,7 @@ export const actions = {
 			const failedTags: { tag: string; unknownWords: string[] }[] = [];
 
 			for (const tag of entryTags) {
-				const unknownWords = tag.split("-").filter((part) =>
-					!dictionary.has(part)
-				);
+				const unknownWords = tag.split("-").filter((part) => !dictionary.has(part));
 
 				if (unknownWords.length > 0) {
 					failedTags.push({ tag, unknownWords });
@@ -190,11 +182,9 @@ export const actions = {
 					.onConflictDoNothing();
 
 				return formfail({
-					tag: `Unknown word${failedTags.length === 1 ? "" : "s"}: ${
-						conjunctionFormatter.format(
-							failedTags.flatMap(({ unknownWords }) => unknownWords),
-						)
-					}`,
+					tag: `Unknown word${failedTags.length === 1 ? "" : "s"}: ${conjunctionFormatter.format(
+						failedTags.flatMap(({ unknownWords }) => unknownWords),
+					)}`,
 				});
 			}
 
@@ -283,9 +273,7 @@ export const actions = {
 					eq(entriesToTags.entryUid, entryUid),
 					inArray(
 						entriesToTags.tagId,
-						oldEntryTags.filter((t) => !entryTags.includes(t.name)).map((t) =>
-							t.id
-						),
+						oldEntryTags.filter((t) => !entryTags.includes(t.name)).map((t) => t.id),
 					),
 				),
 			);
