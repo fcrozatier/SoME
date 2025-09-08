dc6a0a8903c7a60a4e17584c5120f9f9fb86eb8b
fcrozatier <frederic.crozatier@protonmail.com>
2025-09-08 13:48:51 +0200

    display ranked entries

--- src/routes/admin/rank/[category=category]/+page.server.ts
+++ src/routes/admin/rank/[category=category]/+page.server.ts
@@ -5,17 +5,63 @@ import type { SelectEntry } from "$lib/server/db/schema.js";
 import { error } from "@sveltejs/kit";
 import { sql } from "drizzle-orm";
 
-export const load = async ({ params, locals }) => {
+export const load = async ({ params, locals, url }) => {
 	if (!locals.user?.isAdmin) return error(404);
 
 	const { category } = params;
 
-	const entries: Pick<SelectEntry, "title" | "rank">[] = await db.execute(sql`
-		 select title, rank from entries
-		 where active='t'
-		 and date_part('year', entries.created_at)=${currentYear}
-		 and category=${category}
-		 order by rank asc
+	let page = url.searchParams.get("page");
+
+	if (!page) {
+		url.searchParams.set("page", "1");
+		page = "1";
+	}
+
+	const limit = 50;
+
+	const entries:
+		(Pick<SelectEntry, "uid" | "title" | "description" | "category" | "url"> & {
+			overall_median: number | null;
+			teacher_median: number | null;
+			ranking: string;
+			pages: number;
+		})[] = await db.execute(sql`
+		with
+			overall_score as (
+				select entry_uid, percentile_cont(0.5) within group (order by score) as median
+				from votes
+				where date_part('year', created_at)=${currentYear}
+				group by entry_uid
+			),
+
+			teacher_score as (
+				select entry_uid, percentile_cont(0.5) within group (order by score) as median
+				from votes join users on votes.user_uid=users.uid
+				where date_part('year', votes.created_at)=${currentYear}
+				and is_teacher='t'
+				group by entry_uid
+			),
+
+			rank as (
+				select uid, overall_score.median as overall_median, dense_rank() over (order by overall_score.median desc) as ranking
+				from entries
+				right join overall_score on overall_score.entry_uid=entries.uid
+				where category=${category}
+				and active='t'
+				order by overall_median desc
+			),
+
+			paginated as (
+				select entries.uid, title, description, category, url, ranking, overall_median, teacher_score.median as teacher_median, count(*) over () as total_items
+				from entries
+				right join rank on entries.uid=rank.uid
+				left join teacher_score on entries.uid=teacher_score.entry_uid
+				order by rank asc
+				limit ${limit}
+				offset ${(+page - 1) * limit}
+			)
+
+			select *, ceil(total_items / ${limit}) as pages from paginated;
 		`);
 
 	return { entries };
