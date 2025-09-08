4d9f2cdf624ef00533f44354bbb1fe8a89241578
fcrozatier <frederic.crozatier@protonmail.com>
2025-09-08 11:53:14 +0200

    accept null or undefined

--- src/lib/components/Score.svelte
+++ src/lib/components/Score.svelte
@@ -1,22 +1,26 @@
 <script lang="ts">
 	import { round } from "@fcrozatier/ts-helpers";
 
-	const { score }: { score: number } = $props();
+	const { score }: { score: number | null | undefined } = $props();
 
 	const color = $derived(
-		score < 5
-			? `color-mix(in oklch, var(--color-error), var(--color-warning) ${round((2 * 100 * (score - 1)) / 8)}%)`
-			: `color-mix(in oklch, var(--color-warning), var(--color-success) ${round(2 * ((100 * (score - 1)) / 8 - 50))}%)`,
+		!score
+			? "-"
+			: score < 5
+				? `color-mix(in oklch, var(--color-error), var(--color-warning) ${round((2 * 100 * (score - 1)) / 8)}%)`
+				: `color-mix(in oklch, var(--color-warning), var(--color-success) ${round(2 * ((100 * (score - 1)) / 8 - 50))}%)`,
 	);
 </script>
 
-<div class="w-16 flex justify-center" style={`--color: ${color};`}>
-	<span
-		class={`w-8 border text-xs rounded-full aspect-square min-w-4 text-center px-2 flex items-center justify-center`}
-	>
-		{round(score, 1)}
-	</span>
-</div>
+{#if score}
+	<div class="w-16 flex justify-center" style={`--color: ${color};`}>
+		<span
+			class={`w-8 border text-xs rounded-full aspect-square min-w-4 text-center px-2 flex items-center justify-center`}
+		>
+			{round(score, 1)}
+		</span>
+	</div>
+{/if}
 
 <style>
 	span {
