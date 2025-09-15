17189c3e5cb57bc21b1c8e16a1723d28701bb5cd
fcrozatier <frederic.crozatier@protonmail.com>
2025-09-15 12:09:54 +0200

    prepare survey display

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,3 +1,23 @@
-export const load = ({ locals }) => {
+import { db } from "$lib/server/db/index.js";
+import { sql } from "drizzle-orm";
+
+export const load = async ({ locals, cookies }) => {
+	// const uid = locals.user?.uid;
+
+	// let surveyTaken: boolean = cookies.get("survey") === "true";
+
+	// if (!surveyTaken && uid) {
+	// 	const rows = await db.execute(sql`
+	// 			select surveys.created_at from surveys join users
+	// 			on surveys.user_uid=users.uid
+	// 			where users.uid=${uid}
+	// 			and date_part('year', surveys.created_at)='2025';
+	// 		`);
+
+	// 	if (rows.length > 0) {
+	// 		surveyTaken = true;
+	// 	}
+	// }
+
 	return { ...locals };
 };
