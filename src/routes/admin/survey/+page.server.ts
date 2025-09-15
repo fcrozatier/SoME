37f04440faa90705badc804b32dd6f9a91ce6b80
fcrozatier <frederic.crozatier@protonmail.com>
2025-09-15 12:11:29 +0200

    format

--- src/routes/admin/survey/+page.server.ts
+++ src/routes/admin/survey/+page.server.ts
@@ -7,9 +7,8 @@ import { sql } from "drizzle-orm";
 export const load = async ({ locals }) => {
 	if (!locals.user?.isAdmin) return error(404);
 
-	const surveys:
-		(Pick<SelectSurveys, "some" | "site" | "feedback" | "json">)[] = await db
-			.execute(sql`
+	const surveys: Pick<SelectSurveys, "some" | "site" | "feedback" | "json">[] =
+		await db.execute(sql`
 			select "some", site, feedback, json
 			from surveys
 			where date_part('year', created_at)=${currentYear};
