52fea04cd51d7c1689400f92967f49c524a00f3a
Frédéric Crozatier <48696601+fcrozatier@users.noreply.github.com>
2025-10-10 12:05:20 +0200

    chore: close survey (#107)
    * update packages
    
    * remove banner
    
    * filter survey

--- src/routes/admin/survey/+page.svelte
+++ src/routes/admin/survey/+page.svelte
@@ -1,9 +1,28 @@
 <script lang="ts">
+	import { enhance } from "$app/forms";
+	import { disableSubmitterAndSetValidity } from "$lib/actions";
+
 	let { data } = $props();
 </script>
 
 <article class="mx-auto w-4/5 max-w-5xl overflow-x-auto">
 	<h2>Feedback</h2>
+
+	<!-- <form
+		class="p-1 flex gap-2"
+		action="?/filter"
+		method="post"
+		use:enhance={disableSubmitterAndSetValidity()}
+	>
+		<select class="select-bordered select" name="filter">
+			<option value="feedback">general feedback</option>
+			<option value="peer_review_feedback">peer review feedback (convenient time)</option>
+			<option value="ai_feedback">ai feedback</option>
+			<option value="creator_sharable_vote_link">creator sharable vote link feedback</option>
+			<option value="a11y_feedback">accessibility feedback</option>
+		</select>
+		<button class="btn btn-primary">filter</button>
+	</form> -->
 
 	<ul class="list-none">
 		{#each data.surveys as { json }}
