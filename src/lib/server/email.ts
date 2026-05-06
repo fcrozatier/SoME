81c025fb02aaa0983ffda512c5cb9ae9f7e7943e
fcrozatier <frederic.crozatier@protonmail.com>
2026-05-06 13:33:58 +0200

    fix imports

--- src/lib/server/email.ts
+++ src/lib/server/email.ts
@@ -2,16 +2,27 @@ import formData from "form-data";
 import Mailgun from "mailgun.js";
 import type { MailgunMessageData } from "mailgun.js/definitions";
 import { DOMAIN, MAILGUN_API_KEY } from "$env/static/private";
-import { emailTemplates, type TemplateName } from "$lib/config";
-
-const from = "SoME <some@3blue1brown.com>";
 
 /**
  * https://github.com/mailgun/mailgun.js
  */
 const mailgun = new Mailgun(formData);
 
-export const mg = mailgun.client({ username: "api", key: MAILGUN_API_KEY });
+const mg = mailgun.client({ username: "api", key: MAILGUN_API_KEY });
+
+const from = "SoME <some@3blue1brown.com>";
+
+/**
+ * The value is the template email subject
+ */
+const emailTemplates = {
+	"change-password": {
+		subject: `SoME password reset`,
+		variables: ["token"],
+	},
+} as const;
+
+type TemplateName = keyof typeof emailTemplates;
 
 // https://documentation.mailgun.com/en/latest/api-email-validation.html
 type Validation = {
@@ -20,7 +31,12 @@ type Validation = {
 	is_disposable_address: boolean;
 	is_role_address: boolean;
 	reason: string[];
-	result: "deliverable" | "undeliverable" | "do_not_send" | "catch_all" | "unknown";
+	result:
+		| "deliverable"
+		| "undeliverable"
+		| "do_not_send"
+		| "catch_all"
+		| "unknown";
 	risk: "high" | "medium" | "low" | "unknown";
 	root_address?: string;
 };
