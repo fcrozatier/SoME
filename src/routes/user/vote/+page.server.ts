8d22686bee82533ea81d5156ec8e4387828aef95
Frédéric Crozatier <48696601+fcrozatier@users.noreply.github.com>
2026-05-12 12:08:24 +0200

    fix: first vote computation (#128)

--- src/routes/user/vote/+page.server.ts
+++ src/routes/user/vote/+page.server.ts
@@ -1,3 +1,4 @@
+import { currentYear } from "$lib/config.js";
 import { db } from "$lib/server/db";
 import { redirect } from "@sveltejs/kit";
 import { sql } from "drizzle-orm";
@@ -6,10 +7,10 @@ export const load = async ({ locals }) => {
 	const uid = locals.user?.uid;
 	if (!uid) redirect(302, "/login");
 
-	const [userCache, userPreferences]: { count: string }[] = await db.execute(
+	const [userVotes, userPreferences]: { count: string }[] = await db.execute(
 		sql`
-			select count(*) from cache
-			where user_uid=${uid}
+			select count(*) from votes
+			where user_uid=${uid} and date_part('year', created_at)=${currentYear}
 			union all
 			select count(*) from user_to_tag
 			where user_uid=${uid};
@@ -17,7 +18,7 @@ export const load = async ({ locals }) => {
 	);
 
 	return {
-		firstVote: Number(userCache?.count) === 0,
+		firstVote: Number(userVotes?.count) === 0,
 		hasPreferences: Number(userPreferences?.count) > 0,
 	};
 };
