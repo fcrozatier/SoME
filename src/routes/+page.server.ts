92103ecefebc6c172461682d5e717f63fcc69087
fcrozatier <frederic.crozatier@protonmail.com>
2026-07-25 19:04:11 +0200

    remove winners query

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -13,27 +13,14 @@ export const load = async () => {
 		SelectEntry,
 		"uid" | "title" | "description" | "category" | "thumbnail" | "url"
 	>[] = await db.execute(sql`
-		 select uid, title, description, category, thumbnail, url from entries
+		 select uid, title, description, category, thumbnail, url
+		 from entries
 		 where active='t'
 		 and deleted_at is null
-		 and date_part('year', entries.created_at)=${defaultYear()}
+		 and date_part('year', created_at)=${defaultYear()}
 		 order by final_score desc nulls last
 	   limit 5;
 		`);
-
-	// const winners: Pick<
-	// 	SelectEntry,
-	// 	"uid" | "title" | "description" | "category" | "thumbnail" | "url"
-	// >[] = await db.execute(sql`
-	// 	 select uid, title, description, category, thumbnail, url from entries
-	// 	 where uid in (
-	// 	 	'e55b9384-641c-4709-8aef-3447c8d25e80',
-	// 		'6ed691c3-cfef-49c9-b1ed-2adf66536c0b',
-	// 		'c46f3c19-f1e2-402e-af55-8b4b61c508b3',
-	// 		'd4dd701c-240a-48d3-ac87-cbba560afee5',
-	// 		'b8300cb6-9de1-44d4-ac2a-fd1ca5b20819'
-	// 		);
-	// 	`);
 
 	return { top };
 };
