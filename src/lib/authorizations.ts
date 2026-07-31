518e18873cb4bee3589774d9a944d8b79869980e
fcrozatier <frederic.crozatier@protonmail.com>
2026-07-31 15:04:11 +0200

    improve authorization types

--- src/lib/authorizations.ts
+++ src/lib/authorizations.ts
@@ -1,5 +1,7 @@
 import { error } from "@sveltejs/kit";
 
-export function assertIsAdmin(locals: App.Locals): asserts locals is App.Locals & { user: {} } {
+export function assertIsAdmin(
+	locals: App.Locals,
+): asserts locals is App.Locals & { user: { isAdmin: true } } {
 	if (!locals.user?.isAdmin) return error(403);
 }
