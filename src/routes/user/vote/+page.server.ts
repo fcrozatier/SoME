d5763a747af1f7e58481a7ea7898dd2be2ad9925
Frédéric Crozatier <48696601+fcrozatier@users.noreply.github.com>
2026-08-05 14:57:57 +0200

    fix: remove top level query (#151)

--- src/routes/user/vote/+page.server.ts
+++ src/routes/user/vote/+page.server.ts
@@ -1,30 +1,30 @@
-import { assertIsLoggedIn } from "$lib/server/authorization.js";
 import { CURRENT_YEAR } from "$lib/constants.js";
+import { assertIsLoggedIn } from "$lib/server/authorization.js";
 import { db } from "$lib/server/db";
 import { sql } from "drizzle-orm";
-
-const [nbEntries]: { count: string }[] = await db.execute(sql`
-		select count(*) from entries
-		where date_part('year', created_at)=${CURRENT_YEAR};
-	`);
 
 export const load = async ({ locals }) => {
 	assertIsLoggedIn(locals);
 	const uid = locals.user?.uid;
 
-	const [userVotes, userPreferences]: { count: string }[] = await db.execute(
+	const [userVotes, userPreferences]: { count: number }[] = await db.execute(
 		sql`
-			select count(*) from votes
+			select count(*)::int from votes
 			where user_uid=${uid} and date_part('year', created_at)=${CURRENT_YEAR}
 			union all
-			select count(*) from user_to_tag
+			select count(*)::int from user_to_tag
 			where user_uid=${uid};
     `,
 	);
 
+	const [nbEntries]: { count: number }[] = await db.execute(sql`
+			select count(*)::int from entries
+			where date_part('year', created_at)=${CURRENT_YEAR};
+		`);
+
 	return {
-		firstVote: Number(userVotes?.count) === 0,
-		hasPreferences: Number(userPreferences?.count) > 0,
-		nbEntries: Number(nbEntries?.count),
+		firstVote: userVotes?.count === 0,
+		hasPreferences: userPreferences?.count !== 0,
+		nbEntries: nbEntries?.count,
 	};
 };
