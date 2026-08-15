10b1b20eb12629bd018f32880b4e15bf4fab4ed8
fcrozatier <frederic.crozatier@protonmail.com>
2026-08-15 16:21:21 +0200

    only update current entry

--- src/routes/user/votes/+page.server.ts
+++ src/routes/user/votes/+page.server.ts
@@ -1,22 +1,27 @@
+import { CURRENT_YEAR } from "$lib/constants";
 import { assertIsLoggedIn } from "$lib/server/authorization.js";
 import { db } from "$lib/server/db/index.js";
 import type { SelectEntry, SelectVote } from "$lib/server/db/schema";
 import { parseAndSanitizeMarkdown } from "$lib/utils/markdown.js";
 import { voteOpen } from "$lib/utils/time.js";
 import { VoteSchema } from "$lib/validation";
-import { type Actions, error, redirect } from "@sveltejs/kit";
+import { type Actions, error } from "@sveltejs/kit";
 import { sql } from "drizzle-orm";
 import { formgate } from "formgator/sveltekit";
 
 export const load = async ({ locals }) => {
 	assertIsLoggedIn(locals);
 
-	const votes: (Pick<
-		SelectEntry,
-		"uid" | "title" | "description" | "category" | "url" | "thumbnail"
-	> &
-		Pick<SelectVote, "score" | "feedback_unsafe_md" | "feedback" | "created_at">)[] =
-		await db.execute(sql`
+	const votes: (
+		& Pick<
+			SelectEntry,
+			"uid" | "title" | "description" | "category" | "url" | "thumbnail"
+		>
+		& Pick<
+			SelectVote,
+			"score" | "feedback_unsafe_md" | "feedback" | "created_at"
+		>
+	)[] = await db.execute(sql`
       select score, feedback, feedback_unsafe_md, votes.created_at, uid, title, description, category, url, thumbnail
       from votes join entries on entry_uid=uid
       where user_uid=${locals.user.uid};`);
@@ -37,7 +42,8 @@ export const actions: Actions = {
 		await db.execute(
 			sql`update votes
       set score=${data.score}, feedback=${feedbackSafe}, feedback_unsafe_md=${data.feedback}
-      where (user_uid, entry_uid)=(${locals.user.uid}, ${data.uid});`,
+      where (user_uid, entry_uid)=(${locals.user.uid}, ${data.uid})
+			and date_part('year', created_at)=${CURRENT_YEAR};`,
 		);
 
 		return { success: true };
