9ba4396c2e27c8e355e33e1201c9db275660d4f5
fcrozatier <frederic.crozatier@protonmail.com>
2026-08-19 20:29:59 +0200

    fix

--- src/routes/user/watchlist/+page.server.ts
+++ src/routes/user/watchlist/+page.server.ts
@@ -1,9 +1,10 @@
+import { ENTRY_STATE } from "$lib/constants.js";
 import { assertIsLoggedIn } from "$lib/server/authorization.js";
 import { db } from "$lib/server/db/index.js";
 import type { SelectEntry } from "$lib/server/db/schema";
 import { voteOpen } from "$lib/utils/time.js";
 import { SkipSchema } from "$lib/validation";
-import { type Actions, error, redirect } from "@sveltejs/kit";
+import { type Actions, error } from "@sveltejs/kit";
 import { sql } from "drizzle-orm";
 import { formgate } from "formgator/sveltekit";
 
@@ -16,7 +17,9 @@ export const load = async ({ locals }) => {
 	>[] = await db.execute(sql`
       select uid, title, description, category, url, thumbnail
       from user_to_watchlist join entries on user_to_watchlist.entry_uid=entries.uid
-      where user_uid=${locals.user.uid};`);
+      where user_uid=${locals.user.uid}
+			and entries.state in ${[ENTRY_STATE.Active, ENTRY_STATE.Flagged]}
+			;`);
 
 	return { watchlist };
 };
